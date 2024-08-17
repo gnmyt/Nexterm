16616fabece20bb34e7258a76dd90f1739ed2b09
Mathias Wagner <germannewsmaker@gmail.com>
2024-08-17 11:37:22 +0200

    Upload vite client template

---
+++ client/src/main.jsx
@@ -0,0 +1,8 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <h1>App running</h1>
+    </StrictMode>,
+);
