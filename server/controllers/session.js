b1c2a5c57bc41f886228f530b61e4fd5ea318341
Mathias Wagner <germannewsmaker@gmail.com>
2024-08-18 14:15:33 +0200

    Create session.js controller

---
+++ server/controllers/session.js
@@ -0,0 +1,18 @@
+const Session = require("../models/Session");
+
+module.exports.listSessions = async accountId => {
+    return (await Session.findAll({ where: { accountId } }))
+        .sort((a, b) => b.lastActivity - a.lastActivity)
+        .map(session => ({ id: session.id, ip: session.ip, userAgent: session.userAgent, lastActivity: session.lastActivity }));
+}
+
+module.exports.destroySession = async (accountId, sessionId) => {
+    const session = await Session.findOne({ where: { accountId, id: sessionId } });
+
+    if (session === null)
+        return { code: 206, message: "The provided session does not exist" };
+
+    await Session.destroy({ where: { accountId, id: sessionId } });
+
+    return { message: "The session has been successfully destroyed" };
+};
