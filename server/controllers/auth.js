9ee2adadcccd6b295962c7006f6b84f46365b3c7
Mathias Wagner <germannewsmaker@gmail.com>
2024-08-18 14:15:29 +0200

    Create auth.js controller

---
+++ server/controllers/auth.js
@@ -0,0 +1,50 @@
+const Account = require("../models/Account");
+const Session = require("../models/Session");
+const speakeasy = require("speakeasy");
+const { compare } = require("bcrypt");
+
+module.exports.login = async (configuration, user) => {
+    const account = await Account.findOne({ where: { username: configuration.username } });
+
+    // Check if account exists
+    if (account === null)
+        return { code: 201, message: "Username or password incorrect" };
+
+    // Check if password is correct
+    if (!(await compare(configuration.password, account.password)))
+        return { code: 201, message: "Username or password incorrect" };
+
+    // Check if TOTP is required
+    if (account.totpEnabled && !configuration.code)
+        return { code: 202, message: "TOTP is required for this account" };
+
+    // Check if TOTP is correct
+    if (account.totpEnabled) {
+        const tokenCorrect = speakeasy.totp.verify({
+            secret: account.totpSecret || "",
+            encoding: "base32",
+            token: configuration.code,
+        });
+
+        if (!tokenCorrect)
+            return { code: 203, message: "Your provided code is invalid or has expired." };
+    }
+
+    // Create Session
+    const session = await Session.create({
+        accountId: account.id,
+        ip: user.ip,
+        userAgent: user.userAgent,
+    });
+
+    return { token: session.token, totpRequired: account.totpEnabled };
+};
+
+module.exports.logout = async token => {
+    const session = await Session.findOne({ where: { token } });
+
+    if (session === null)
+        return { code: 204, message: "Your session token is invalid" };
+
+    await Session.destroy({ where: { token } });
+};
