2c863b7f4edb3d6361cce53493a9fec4d7d32901
Mathias Wagner <germannewsmaker@gmail.com>
2024-08-18 14:17:09 +0200

    Create session.js router

---
+++ server/routes/session.js
@@ -0,0 +1,14 @@
+const { Router } = require("express");
+const { listSessions, destroySession } = require("../controllers/session");
+
+const app = Router();
+
+app.get("/list", async (req, res) => {
+    res.json(await listSessions(req.user.id));
+});
+
+app.delete("/:id", async (req, res) => {
+    res.json(await destroySession(req.user.id, req.params.id));
+});
+
+module.exports = app;
