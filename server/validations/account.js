31cc1b16ef7eada9073d442d09b5a5af380e799b
Mathias Wagner <germannewsmaker@gmail.com>
2024-08-18 14:16:08 +0200

    Create account.js validation

---
+++ server/validations/account.js
@@ -0,0 +1,21 @@
+const Joi = require('joi');
+
+module.exports.registerValidation = Joi.object({
+    username: Joi.string().min(3).max(15).alphanum().required(),
+    password: Joi.string().min(5).max(150).required(),
+    firstName: Joi.string().min(2).max(50).required(),
+    lastName: Joi.string().min(2).max(50).required(),
+});
+
+module.exports.totpSetup = Joi.object({
+    code: Joi.number().integer().required(),
+});
+
+module.exports.passwordChangeValidation = Joi.object({
+    password: Joi.string().min(5).max(150).required(),
+});
+
+module.exports.updateNameValidation = Joi.object({
+    firstName: Joi.string().min(2).max(50),
+    lastName: Joi.string().min(2).max(50),
+}).or('firstName', 'lastName');
