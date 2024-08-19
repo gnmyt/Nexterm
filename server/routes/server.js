fb37e79927d653c15f325b16aa11d23c4545df10
Mathias Wagner <germannewsmaker@gmail.com>
2024-08-19 15:55:43 +0200

    Create server.js routes

---
+++ server/routes/server.js
@@ -0,0 +1,44 @@
+const { Router } = require("express");
+const { createServer, deleteServer, editServer, getServer, listServers } = require("../controllers/server");
+const { createServerValidation, updateServerValidation } = require("../validations/server");
+const { validateSchema } = require("../utils/schema");
+
+const app = Router();
+
+app.get("/list", async (req, res) => {
+    res.json(await listServers(req.user.id));
+});
+
+app.get("/:serverId", async (req, res) => {
+    const server = await getServer(req.user.id, req.params.serverId);
+    if (server?.code) return res.json(server);
+
+    res.json(server);
+});
+
+app.put("/", async (req, res) => {
+    if (validateSchema(res, createServerValidation, req.body)) return;
+
+    const server = await createServer(req.user.id, req.body);
+    if (server?.code) return res.json(server);
+
+    res.json({ message: "Server got successfully created", id: server.id });
+});
+
+app.delete("/:serverId", async (req, res) => {
+    const server = await deleteServer(req.user.id, req.params.serverId);
+    if (server?.code) return res.json(server);
+
+    res.json({ message: "Server got successfully deleted" });
+});
+
+app.patch("/:serverId", async (req, res) => {
+    if (validateSchema(res, updateServerValidation, req.body)) return;
+
+    const server = await editServer(req.user.id, req.params.serverId, req.body);
+    if (server?.code) return res.json(server);
+
+    res.json({ message: "Server got successfully edited" });
+});
+
+module.exports = app;
