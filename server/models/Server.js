be3e7db77d548c528c65f5aba7ce8cd492e64e67
Mathias Wagner <germannewsmaker@gmail.com>
2024-08-19 15:55:21 +0200

    Create Server.js model

---
+++ server/models/Server.js
@@ -0,0 +1,37 @@
+const Sequelize = require("sequelize");
+const db = require("../utils/database");
+
+module.exports = db.define("servers", {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    accountId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
+    folderId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
+    icon: {
+        type: Sequelize.STRING,
+        allowNull: true,
+    },
+    protocol: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    ip: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    port: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
+    identities: {
+        type: Sequelize.JSON,
+        defaultValue: [],
+    },
+}, { freezeTableName: true, createdAt: false, updatedAt: false });
