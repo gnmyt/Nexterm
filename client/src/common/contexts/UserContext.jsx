0b233722cbac42fe90b44c6ffc72141f61003fb0
Mathias Wagner <germannewsmaker@gmail.com>
2024-08-19 21:59:34 +0200

    Create UserContext.jsx

---
+++ client/src/common/contexts/UserContext.jsx
@@ -0,0 +1,44 @@
+import { createContext, useEffect, useState } from "react";
+import LoginDialog from "@/common/components/LoginDialog/index.js";
+import { getRequest } from "@/common/utils/RequestUtil.js";
+
+export const UserContext = createContext({});
+
+export const UserProvider = ({ children }) => {
+
+    const [sessionToken, setSessionToken] = useState(localStorage.getItem("sessionToken"));
+    const [user, setUser] = useState();
+
+    const updateSessionToken = (sessionToken) => {
+        setSessionToken(sessionToken);
+        localStorage.setItem("sessionToken", sessionToken);
+
+        login();
+    }
+
+    const login = async () => {
+        try {
+            const userObj = await getRequest("accounts/me");
+            setUser(userObj);
+        } catch (error) {
+            if (error.message === "Unauthorized") {
+                setSessionToken(null);
+                localStorage.removeItem("sessionToken");
+            }
+            return;
+        }
+    }
+
+    useEffect(() => {
+        if (sessionToken) {
+            login();
+        }
+    }, []);
+
+    return (
+        <UserContext.Provider value={{updateSessionToken, user}}>
+            <LoginDialog open={!sessionToken} />
+            {children}
+        </UserContext.Provider>
+    );
+}
