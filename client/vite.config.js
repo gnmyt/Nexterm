df7d29e9ea03b702a723702f27d4228c2becaf3b
Mathias Wagner <germannewsmaker@gmail.com>
2024-08-19 19:10:01 +0200

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import * as path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src"),
+        }
+    },
+    server: {
+        proxy: {
+            "/api": "http://localhost:6989",
+        }
+    }
 });
