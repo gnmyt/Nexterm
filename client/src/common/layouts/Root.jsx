3b1dd4e269854e4c6465d355662293a20bc3cfd0
Mathias Wagner <germannewsmaker@gmail.com>
2024-08-19 21:59:45 +0200

    Implement UserContext into Root.jsx layout

--- client/src/common/layouts/Root.jsx
+++ client/src/common/layouts/Root.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "@/common/components/Sidebar";
+import { UserProvider } from "@/common/contexts/UserContext.jsx";
 
 export default () => {
     return (
         <div className="root">
-            <Sidebar />
-            <Outlet />
+            <UserProvider>
+                <Sidebar />
+                <Outlet />
+            </UserProvider>
         </div>
     )
 }
