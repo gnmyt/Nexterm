2d383e83e25c4f86a9a604b2d217488700a521c2
Mathias Wagner <germannewsmaker@gmail.com>
2024-08-19 22:00:26 +0200

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import App from "@/App.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <h1>App running</h1>
+        <App />
     </StrictMode>,
 );
