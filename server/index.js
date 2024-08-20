aff572f80320fd0109029b69f44a6ad90d734555
Mathias Wagner <germannewsmaker@gmail.com>
2024-08-21 01:56:13 +0200

    Implement guacd into index.js

--- server/index.js
+++ server/index.js
@@ -1,16 +1,22 @@
-const express = require('express');
-const path = require('path');
-const db = require("./utils/database");
-const { authenticate } = require('./middlewares/auth');
-require('./utils/folder'); // Create needed data folders
+const crypto = require("crypto");
+module.exports.GUACD_TOKEN = crypto.randomBytes(16).toString("hex");
 
-process.on('uncaughtException', err => require('./utils/errorHandling')(err));
+const express = require("express");
+const path = require("path");
+const db = require("./utils/database");
+const { authenticate, authorizeGuacamole } = require("./middlewares/auth");
+const GuacamoleLite = require("guacamole-lite");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+require("./utils/folder");  // Create needed data folders
+
+process.on("uncaughtException", err => require("./utils/errorHandling")(err));
 
 const APP_PORT = process.env.SERVER_PORT || 6989;
 
 const app = express();
 
-app.disable('x-powered-by');
+app.disable("x-powered-by");
 
 app.use(express.json());
 
@@ -22,21 +28,36 @@ app.use("/api/folders", authenticate, require("./routes/folder"));
 app.use("/api/servers", authenticate, require("./routes/server"));
 app.use("/api/identities", authenticate, require("./routes/identity"));
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../dist')));
+new GuacamoleLite({ port: 58391 }, { port: 4822 }, {
+    crypt: { cypher: "AES-256-CBC", key: module.exports.GUACD_TOKEN },
+    log: { level: 0 },
+});
 
-    app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../dist', 'index.html')));
+app.use("/servers/guacd", createProxyMiddleware({
+    changeOrigin: true,
+    ws: true,
+    router: async (req) => {
+        const token = await authorizeGuacamole(req);
+        if (!token) return null;
+        return "ws://localhost:" + 58391 + "/?token=" + token;
+    },
+}));
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../dist")));
+
+    app.get("*", (req, res) => res.sendFile(path.join(__dirname, "../dist", "index.html")));
 } else {
-    app.get("*", (req, res) => res.status(500).sendFile(path.join(__dirname, 'templates', 'env.html')));
+    app.get("*", (req, res) => res.status(500).sendFile(path.join(__dirname, "templates", "env.html")));
 }
 
-db.authenticate().then(async () => {
+db.authenticate().catch(err => {
+    console.error("Could not open the database file. Maybe it is damaged?: " + err.message);
+    process.exit(111);
+}).then(async () => {
     console.log("Successfully connected to the database " + (process.env.DB_TYPE === "mysql" ? "server" : "file"));
 
     await db.sync({ alter: true, force: false });
 
     app.listen(APP_PORT, () => console.log(`Server listening on port ${APP_PORT}`));
-}).catch(err => {
-    console.error("Could not open the database file. Maybe it is damaged?: " + err.message);
-    process.exit(111);
 });
