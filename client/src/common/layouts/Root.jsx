b5d4fa55e20f78898f835ef5b30ace1f21585128
Mathias Wagner <germannewsmaker@gmail.com>
2024-08-20 21:31:33 +0200

    Implement IdentityContext into Root.jsx layout

--- client/src/common/layouts/Root.jsx
+++ client/src/common/layouts/Root.jsx
@@ -2,16 +2,19 @@ import { Outlet } from "react-router-dom";
 import Sidebar from "@/common/components/Sidebar";
 import { UserProvider } from "@/common/contexts/UserContext.jsx";
 import { ServerProvider } from "@/common/contexts/ServerContext.jsx";
+import { IdentityProvider } from "@/common/contexts/IdentityContext.jsx";
 
 export default () => {
     return (
         <div className="root">
             <UserProvider>
                 <ServerProvider>
-                    <div className="content-wrapper">
-                        <Sidebar />
-                        <Outlet />
-                    </div>
+                    <IdentityProvider>
+                        <div className="content-wrapper">
+                            <Sidebar />
+                            <Outlet />
+                        </div>
+                    </IdentityProvider>
                 </ServerProvider>
             </UserProvider>
         </div>
