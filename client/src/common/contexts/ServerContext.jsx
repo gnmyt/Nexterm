3911c528fc2196d163f6e7a78761a3ffb1e2e26b
Mathias Wagner <germannewsmaker@gmail.com>
2024-08-20 13:53:00 +0200

    Create ServerContext.jsx

--- client/src/common/contexts/ServerContext.jsx
+++ client/src/common/contexts/ServerContext.jsx
@@ -1,10 +1,39 @@
-import { createContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { UserContext } from "@/common/contexts/UserContext.jsx";
+import { getRequest } from "@/common/utils/RequestUtil.js";
 
-export const ServerContext = createContext({  });
+export const ServerContext = createContext({});
 
 export const ServerProvider = ({ children }) => {
+
+    const [servers, setServers] = useState(null);
+
+    const {user} = useContext(UserContext);
+
+    const loadServers = async () => {
+        try {
+            getRequest("/servers/list").then((response) => {
+                setServers(response);
+            });
+        } catch (error) {
+            console.error("Failed to load servers", error.message);
+        }
+    }
+
+    useEffect(() => {
+        if (user) {
+            loadServers();
+
+            const interval = setInterval(() => {
+                loadServers();
+            }, 5000);
+
+            return () => clearInterval(interval);
+        }
+    }, [user]);
+
     return (
-        <ServerContext.Provider value={{  }}>
+        <ServerContext.Provider value={{servers, loadServers}}>
             {children}
         </ServerContext.Provider>
     )
