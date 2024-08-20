5d5052270b41986d12300b370d690752a1b4b154
Mathias Wagner <germannewsmaker@gmail.com>
2024-08-20 20:33:03 +0200

    Update Servers.jsx

--- client/src/pages/Servers/Servers.jsx
+++ client/src/pages/Servers/Servers.jsx
@@ -10,12 +10,22 @@ import ServerDialog from "@/pages/Servers/components/ServerDialog";
 export const Servers = () => {
 
     const [serverDialogOpen, setServerDialogOpen] = useState(false);
+    const [currentFolderId, setCurrentFolderId] = useState(null);
+    const [editServerId, setEditServerId] = useState(null);
     const {user} = useContext(UserContext);
+
+    const closeDialog = () => {
+        setServerDialogOpen(false);
+        setCurrentFolderId(null);
+        setEditServerId(null);
+    }
 
     return (
         <div className="server-page">
-            <ServerDialog open={serverDialogOpen} onClose={() => setServerDialogOpen(false)} />
-            <ServerList setServerDialogOpen={() => setServerDialogOpen(true)} />
+            <ServerDialog open={serverDialogOpen} onClose={closeDialog} currentFolderId={currentFolderId}
+                            editServerId={editServerId}/>
+            <ServerList setServerDialogOpen={() => setServerDialogOpen(true)}
+                        setCurrentFolderId={setCurrentFolderId} setEditServerId={setEditServerId}/>
             <div className="welcome-area">
                 <div className="area-left">
                     <h1>Hi, <span>{user?.firstName || "User"} {user?.lastName || "name"}</span>!</h1>
