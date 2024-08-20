c68dd9585738d332e957c2a240150784aa321889
Mathias Wagner <germannewsmaker@gmail.com>
2024-08-20 13:53:23 +0200

    Implement ServerProvider in Root.jsx

--- client/src/common/layouts/Root.jsx
+++ client/src/common/layouts/Root.jsx
@@ -1,13 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "@/common/components/Sidebar";
 import { UserProvider } from "@/common/contexts/UserContext.jsx";
+import { ServerProvider } from "@/common/contexts/ServerContext.jsx";
 
 export default () => {
     return (
         <div className="root">
             <UserProvider>
-                <Sidebar />
-                <Outlet />
+                <ServerProvider>
+                    <div className="content-wrapper">
+                        <Sidebar />
+                        <Outlet />
+                    </div>
+                </ServerProvider>
             </UserProvider>
         </div>
     )
