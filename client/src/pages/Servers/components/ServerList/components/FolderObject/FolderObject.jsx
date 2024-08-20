32bcb253f3de7cc0ec7838344a5a1269dfae98db
Mathias Wagner <germannewsmaker@gmail.com>
2024-08-20 16:15:34 +0200

    Update FolderObject

--- client/src/pages/Servers/components/ServerList/components/FolderObject/FolderObject.jsx
+++ client/src/pages/Servers/components/ServerList/components/FolderObject/FolderObject.jsx
@@ -1,13 +1,47 @@
 import Icon from "@mdi/react";
 import { mdiFolderOpenOutline, mdiFolderOutline } from "@mdi/js";
 import "./styles.sass";
+import { useContext, useEffect, useRef, useState } from "react";
+import { patchRequest } from "@/common/utils/RequestUtil.js";
+import { ServerContext } from "@/common/contexts/ServerContext.jsx";
 
-export const FolderObject = ({ id, name, nestedLevel, onClick, isOpen }) => {
+export const FolderObject = ({ id, name, nestedLevel, onClick, isOpen, renameState, setRenameStateId }) => {
+    const inputRef = useRef();
+
+    const {loadServers} = useContext(ServerContext);
+    const [nameState, setNameState] = useState(name || "");
+
+    const changeName = () => {
+        setNameState(name => {
+            patchRequest("folders/" + id + "/rename", { name }).then(() => {
+                loadServers();
+                setRenameStateId(null);
+            });
+
+            return name;
+        });
+    }
+
+    useEffect(() => {
+        if (renameState) {
+            inputRef.current?.focus();
+            inputRef.current?.select();
+
+            const handleEnter = (e) => {
+                if (e.key === "Enter") changeName();
+            }
+
+            document.addEventListener("keydown", handleEnter);
+            return () => document.removeEventListener("keydown", handleEnter);
+        }
+    }, [renameState]);
     return (
-        <div className="folder-object" style={{ paddingLeft: `${10 + (nestedLevel * 15)}px` }} onClick={onClick}
-             data-id={id}>
+        <div className="folder-object" style={{ paddingLeft: `${10 + (nestedLevel * 15)}px` }} data-id={id}
+             onClick={renameState ? (e) => e.stopPropagation() : onClick}>
             <Icon path={isOpen ? mdiFolderOpenOutline : mdiFolderOutline} />
-            <p>{name}</p>
+            {!renameState && <p>{nameState}</p>}
+            {renameState && <input type="text" ref={inputRef} value={nameState} onBlur={changeName}
+                                   onChange={(e) => setNameState(e.target.value)} />}
         </div>
     );
 };
