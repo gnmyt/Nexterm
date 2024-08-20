b5854a821fb3b7d6c16e13640db67eda8ccab051
Mathias Wagner <germannewsmaker@gmail.com>
2024-08-20 15:22:25 +0200

    Add isOpen & id to FolderObject

--- client/src/pages/Servers/components/ServerList/components/FolderObject/FolderObject.jsx
+++ client/src/pages/Servers/components/ServerList/components/FolderObject/FolderObject.jsx
@@ -1,12 +1,13 @@
 import Icon from "@mdi/react";
-import { mdiFolderOutline } from "@mdi/js";
+import { mdiFolderOpenOutline, mdiFolderOutline } from "@mdi/js";
 import "./styles.sass";
 
-export const FolderObject = ({name, nestedLevel, onClick}) => {
+export const FolderObject = ({ id, name, nestedLevel, onClick, isOpen }) => {
     return (
-        <div className="folder-object" style={{paddingLeft: `${10 + (nestedLevel * 15)}px`}} onClick={onClick}>
-            <Icon path={mdiFolderOutline} />
+        <div className="folder-object" style={{ paddingLeft: `${10 + (nestedLevel * 15)}px` }} onClick={onClick}
+             data-id={id}>
+            <Icon path={isOpen ? mdiFolderOpenOutline : mdiFolderOutline} />
             <p>{name}</p>
         </div>
-    )
-}
+    );
+};
