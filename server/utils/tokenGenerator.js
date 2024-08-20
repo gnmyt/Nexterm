fa2ebaa92dfb8c3fc43bd8598f9973d3d4564c4d
Mathias Wagner <germannewsmaker@gmail.com>
2024-08-21 01:53:48 +0200

    Create tokenGenerator.js util

---
+++ server/utils/tokenGenerator.js
@@ -0,0 +1,27 @@
+const crypto = require("crypto");
+const { GUACD_TOKEN } = require("../index");
+
+const encryptToken = (value) => {
+    const iv = crypto.randomBytes(16);
+    const cipher = crypto.createCipheriv("aes-256-cbc", Buffer.from(GUACD_TOKEN), iv);
+
+    let encrypted = cipher.update(JSON.stringify(value), "utf8", "base64");
+    encrypted += cipher.final("base64");
+
+    const data = { iv: iv.toString("base64"), value: encrypted };
+
+    const json = JSON.stringify(data);
+    return Buffer.from(json).toString("base64");
+};
+
+module.exports.createVNCToken = (hostname, port, username, password) => {
+    return encryptToken({ connection: { type: "vnc", settings: { hostname, port, username, password } } });
+};
+
+module.exports.createRDPToken = (hostname, port, username, password) => {
+    return encryptToken({
+        connection: {
+            type: "rdp", settings: { hostname, username, port, password, "ignore-cert": true },
+        },
+    });
+};
