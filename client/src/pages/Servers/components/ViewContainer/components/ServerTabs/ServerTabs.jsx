5a5c9ac79b9d711bbab7fa104ada6bf098dce6fb
Mathias Wagner <germannewsmaker@gmail.com>
2024-08-21 03:55:27 +0200

    Create ServerTabs component

--- client/src/pages/Servers/components/ViewContainer/components/ServerTabs/ServerTabs.jsx
+++ client/src/pages/Servers/components/ViewContainer/components/ServerTabs/ServerTabs.jsx
@@ -1,7 +1,34 @@
-export const ServerTabs = () => {
+import { ServerContext } from "@/common/contexts/ServerContext.jsx";
+import { useContext } from "react";
+import Icon from "@mdi/react";
+import { loadIcon } from "@/pages/Servers/components/ServerList/components/ServerObject/ServerObject.jsx";
+import { mdiClose, mdiTableColumnPlusAfter } from "@mdi/js";
+import "./styles.sass";
+
+export const ServerTabs = ({activeSessions, setActiveSessionId, activeSessionId, disconnectFromServer}) => {
+
+    const {getServerById} = useContext(ServerContext);
+
     return (
         <div className="server-tabs">
-
+            <Icon path={mdiTableColumnPlusAfter} />
+            <div className="tabs">
+                {activeSessions.map(session => {
+                    const server = getServerById(session.server);
+                    return (
+                        <div key={session.id} className={"server-tab" + (session.id === activeSessionId ? " server-tab-active" : "")}
+                                onClick={() => setActiveSessionId(session.id)}>
+                            <Icon path={loadIcon(server.icon)} />
+                            <h2>{server.name}</h2>
+                            <Icon path={mdiClose} onClick={(e) => {
+                                e.stopPropagation();
+                                disconnectFromServer(session.id);
+                            }} />
+                        </div>
+                    )
+                })}
+            </div>
         </div>
+
     )
 }
