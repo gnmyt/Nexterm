048b774559a01ed7b6763551263059a32f4eef64
Mathias Wagner <germannewsmaker@gmail.com>
2024-08-21 03:56:08 +0200

    Update Servers.jsx

--- client/src/pages/Servers/Servers.jsx
+++ client/src/pages/Servers/Servers.jsx
@@ -6,6 +6,7 @@ import Button from "@/common/components/Button";
 import WelcomeImage from "@/common/img/welcome.png";
 import { GITHUB_URL } from "@/App.jsx";
 import ServerDialog from "@/pages/Servers/components/ServerDialog";
+import ViewContainer from "@/pages/Servers/components/ViewContainer/index.js";
 
 export const Servers = () => {
 
@@ -13,6 +14,30 @@ export const Servers = () => {
     const [currentFolderId, setCurrentFolderId] = useState(null);
     const [editServerId, setEditServerId] = useState(null);
     const {user} = useContext(UserContext);
+
+    const [activeSessions, setActiveSessions] = useState([]);
+    const [activeSessionId, setActiveSessionId] = useState(null);
+
+    const connectToServer = (server, identity) => {
+        const sessionId = "session-" + (Math.random().toString(36).substring(2, 15))
+        setActiveSessions(activeSessions => [...activeSessions, {server, identity, id: sessionId}]);
+
+        setActiveSessionId(sessionId);
+    }
+
+    const disconnectFromServer = (sessionId) => {
+        setActiveSessions(activeSessions => {
+            const newSessions = activeSessions.filter(session => session.id !== sessionId);
+
+            if (newSessions.length === 0) {
+                setActiveSessionId(null);
+            } else if (sessionId === activeSessionId) {
+                setActiveSessionId(newSessions[newSessions.length - 1].id);
+            }
+
+            return newSessions;
+        });
+    }
 
     const closeDialog = () => {
         setServerDialogOpen(false);
@@ -24,16 +49,18 @@ export const Servers = () => {
         <div className="server-page">
             <ServerDialog open={serverDialogOpen} onClose={closeDialog} currentFolderId={currentFolderId}
                             editServerId={editServerId}/>
-            <ServerList setServerDialogOpen={() => setServerDialogOpen(true)}
+            <ServerList setServerDialogOpen={() => setServerDialogOpen(true)} connectToServer={connectToServer}
                         setCurrentFolderId={setCurrentFolderId} setEditServerId={setEditServerId}/>
-            <div className="welcome-area">
+            {activeSessions.length === 0 &&<div className="welcome-area">
                 <div className="area-left">
                     <h1>Hi, <span>{user?.firstName || "User"} {user?.lastName || "name"}</span>!</h1>
                     <p>Welcome to Nexterm. The open-source server manager for SSH, VNC and RDP.</p>
                     <Button text="Star on GitHub" onClick={() => window.open(GITHUB_URL, "_blank")} />
                 </div>
                 <img src={WelcomeImage} alt="Welcome" />
-            </div>
+            </div>}
+            {activeSessions && <ViewContainer activeSessions={activeSessions} disconnectFromServer={disconnectFromServer}
+                                              activeSessionId={activeSessionId} setActiveSessionId={setActiveSessionId} />}
         </div>
     )
 }
