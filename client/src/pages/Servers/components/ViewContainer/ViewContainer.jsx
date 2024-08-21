68fb3f9ec4e8cec9e3fd7d00b8b41d0e85227a08
Mathias Wagner <germannewsmaker@gmail.com>
2024-08-21 03:56:02 +0200

    Update ViewContainer.jsx

--- client/src/pages/Servers/components/ViewContainer/ViewContainer.jsx
+++ client/src/pages/Servers/components/ViewContainer/ViewContainer.jsx
@@ -1,10 +1,26 @@
 import "./styles.sass";
 import ServerTabs from "./components/ServerTabs";
+import { useContext } from "react";
+import { ServerContext } from "@/common/contexts/ServerContext.jsx";
+import GuacamoleRenderer from "@/pages/Servers/components/ViewContainer/renderer/GuacamoleRenderer.jsx";
 
-export const ViewContainer = () => {
+export const ViewContainer = ({activeSessions, activeSessionId, setActiveSessionId, disconnectFromServer}) => {
+
+    const {getServerById} = useContext(ServerContext);
     return (
         <div className="view-container">
-            <ServerTabs />
+            <ServerTabs activeSessions={activeSessions} setActiveSessionId={setActiveSessionId}
+                        activeSessionId={activeSessionId} disconnectFromServer={disconnectFromServer} />
+
+            {activeSessions.map(session => {
+                const server = getServerById(session.server);
+                return (
+                    <div key={session.id} className={"view" + (session.id === activeSessionId ? " view-active" : "")}>
+                        {(server.protocol === "vnc" || server.protocol === "rdp") &&
+                            <GuacamoleRenderer session={session} disconnectFromServer={disconnectFromServer} />}
+                    </div>
+                )
+            })}
         </div>
     )
 }
