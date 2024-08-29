8acca43b375301ed0725b11f7be4a62d77044f54
Mathias Wagner <germannewsmaker@gmail.com>
2024-08-29 11:13:05 +0200

    Add onChange to IconInput.jsx

--- client/src/common/components/IconInput/IconInput.jsx
+++ client/src/common/components/IconInput/IconInput.jsx
@@ -2,13 +2,13 @@ import "./styles.sass";
 import Icon from "@mdi/react";
 
 export const IconInput = ({ type, id, name, required, icon, placeholder,
-                              autoComplete, value, setValue }) => {
+                              autoComplete, value, setValue, onChange }) => {
     return (
         <div className="input-container">
             <Icon path={icon} className="input-icon" />
             <input type={type} id={id} name={name} required={required} className="input"
-                     placeholder={placeholder} autoComplete={autoComplete}
-                        value={value} onChange={(event) => setValue(event.target.value)} />
+                     placeholder={placeholder} autoComplete={autoComplete} onInput={onChange}
+                   value={value} onChange={(event) => setValue(event.target.value)} />
         </div>
     );
 };
