5dea88ce30ef530641af6f6a01625aa188718563
Mathias Wagner <germannewsmaker@gmail.com>
2024-08-29 10:08:19 +0200

    Remove json from service.js route

--- server/routes/service.js
+++ server/routes/service.js
@@ -6,7 +6,7 @@ const app = express.Router();
 // Checks if the server is not setup yet (First Time Setup)
 app.get("/is-fts", (req, res) => {
     getFTSStatus()
-        .then(status => res.json({ status }))
+        .then(status => res.json(status))
         .catch(err => res.status(500).json({ error: err.message }));
 });
 
