5a9d48407ebe286a13e07ff599b660204b1d5cf0
Mathias Wagner <germannewsmaker@gmail.com>
2024-08-29 09:48:46 +0200

    Create service.js route

---
+++ server/routes/service.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const { getFTSStatus } = require("../controllers/account");
+
+const app = express.Router();
+
+// Checks if the server is not setup yet (First Time Setup)
+app.get("/is-fts", (req, res) => {
+    getFTSStatus()
+        .then(status => res.json({ status }))
+        .catch(err => res.status(500).json({ error: err.message }));
+});
+
+module.exports = app;
