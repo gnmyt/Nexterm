a596e433d1c59b016a1110cd503a6f5a30a26439
Mathias Wagner <germannewsmaker@gmail.com>
2024-08-29 09:50:14 +0200

    Implement XtermRenderer into ViewContainer.jsx

--- client/src/pages/Servers/components/ViewContainer/ViewContainer.jsx
+++ client/src/pages/Servers/components/ViewContainer/ViewContainer.jsx
@@ -3,6 +3,7 @@ import ServerTabs from "./components/ServerTabs";
 import { useContext } from "react";
 import { ServerContext } from "@/common/contexts/ServerContext.jsx";
 import GuacamoleRenderer from "@/pages/Servers/components/ViewContainer/renderer/GuacamoleRenderer.jsx";
+import XtermRenderer from "@/pages/Servers/components/ViewContainer/renderer/XtermRenderer.jsx";
 
 export const ViewContainer = ({activeSessions, activeSessionId, setActiveSessionId, disconnectFromServer}) => {
 
@@ -18,6 +19,7 @@ export const ViewContainer = ({activeSessions, activeSessionId, setActiveSession
                     <div key={session.id} className={"view" + (session.id === activeSessionId ? " view-active" : "")}>
                         {(server.protocol === "vnc" || server.protocol === "rdp") &&
                             <GuacamoleRenderer session={session} disconnectFromServer={disconnectFromServer} />}
+                        {server.protocol === "ssh" && <XtermRenderer session={session} disconnectFromServer={disconnectFromServer} />}
                     </div>
                 )
             })}
