b55fb89e2638dc20172b5b97936ffa816d749d42
Mathias Wagner <germannewsmaker@gmail.com>
2024-08-30 21:33:10 +0200

    Add customClass & onBlur to IconInput.jsx

--- client/src/common/components/IconInput/IconInput.jsx
+++ client/src/common/components/IconInput/IconInput.jsx
@@ -1,13 +1,14 @@
 import "./styles.sass";
 import Icon from "@mdi/react";
 
-export const IconInput = ({ type, id, name, required, icon, placeholder,
-                              autoComplete, value, setValue, onChange }) => {
+export const IconInput = ({ type, id, name, required, icon, placeholder, customClass,
+                              autoComplete, value, setValue, onChange, onBlur }) => {
     return (
         <div className="input-container">
             <Icon path={icon} className="input-icon" />
-            <input type={type} id={id} name={name} required={required} className="input"
+            <input type={type} id={id} name={name} required={required} className={"input" + customClass}
                      placeholder={placeholder} autoComplete={autoComplete} onInput={onChange}
+                   onBlur={onBlur}
                    value={value} onChange={(event) => setValue(event.target.value)} />
         </div>
     );
