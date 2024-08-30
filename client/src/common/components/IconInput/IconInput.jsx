95dc5dd5511cb8f93c36141f8bd8399f00fb33a9
Mathias Wagner <germannewsmaker@gmail.com>
2024-08-30 21:52:33 +0200

    Fix bug in IconInput.jsx

--- client/src/common/components/IconInput/IconInput.jsx
+++ client/src/common/components/IconInput/IconInput.jsx
@@ -6,7 +6,7 @@ export const IconInput = ({ type, id, name, required, icon, placeholder, customC
     return (
         <div className="input-container">
             <Icon path={icon} className="input-icon" />
-            <input type={type} id={id} name={name} required={required} className={"input" + customClass}
+            <input type={type} id={id} name={name} required={required} className={"input" + (customClass ? customClass : "")}
                      placeholder={placeholder} autoComplete={autoComplete} onInput={onChange}
                    onBlur={onBlur}
                    value={value} onChange={(event) => setValue(event.target.value)} />
