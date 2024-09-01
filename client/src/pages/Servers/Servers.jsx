1a81899a2ff04208ed7c5661795d10c4d8129c0a
Mathias Wagner <germannewsmaker@gmail.com>
2024-09-01 20:06:27 +0200

    Update Servers.jsx

--- client/src/pages/Servers/Servers.jsx
+++ client/src/pages/Servers/Servers.jsx
@@ -7,31 +7,36 @@ import WelcomeImage from "@/common/img/welcome.png";
 import { GITHUB_URL } from "@/App.jsx";
 import ServerDialog from "@/pages/Servers/components/ServerDialog";
 import ViewContainer from "@/pages/Servers/components/ViewContainer";
+import ProxmoxDialog from "@/pages/Servers/components/ProxmoxDialog/index.js";
 
 export const Servers = () => {
 
     const [serverDialogOpen, setServerDialogOpen] = useState(false);
+    const [proxmoxDialogOpen, setProxmoxDialogOpen] = useState(false);
+
     const [currentFolderId, setCurrentFolderId] = useState(null);
     const [editServerId, setEditServerId] = useState(null);
-    const {user} = useContext(UserContext);
+    const { user } = useContext(UserContext);
 
     const [activeSessions, setActiveSessions] = useState([]);
     const [activeSessionId, setActiveSessionId] = useState(null);
 
     const connectToServer = (server, identity) => {
-        const sessionId = "session-" + (Math.random().toString(36).substring(2, 15))
-        setActiveSessions(activeSessions => [...activeSessions, {server, identity, id: sessionId}]);
+        const sessionId = "session-" + (Math.random().toString(36).substring(2, 15));
+        setActiveSessions(activeSessions => [...activeSessions, { server, identity, id: sessionId }]);
 
         setActiveSessionId(sessionId);
-    }
+    };
 
     const connectToPVEServer = (serverId, containerId) => {
-        const sessionId = "session-" + (Math.random().toString(36).substring(2, 15))
-        setActiveSessions(activeSessions => [...activeSessions, {server: serverId.toString().replace("pve-", ""),
-            containerId: containerId.toString().split("-")[containerId.toString().split("-").length - 1], id: sessionId}]);
+        const sessionId = "session-" + (Math.random().toString(36).substring(2, 15));
+        setActiveSessions(activeSessions => [...activeSessions, {
+            server: serverId.toString().replace("pve-", ""),
+            containerId: containerId.toString().split("-")[containerId.toString().split("-").length - 1], id: sessionId,
+        }]);
 
         setActiveSessionId(sessionId);
-    }
+    };
 
     const disconnectFromServer = (sessionId) => {
         setActiveSessions(activeSessions => {
@@ -45,22 +50,25 @@ export const Servers = () => {
 
             return newSessions;
         });
-    }
+    };
 
     const closeDialog = () => {
         setServerDialogOpen(false);
         setCurrentFolderId(null);
         setEditServerId(null);
-    }
+    };
 
     return (
         <div className="server-page">
             <ServerDialog open={serverDialogOpen} onClose={closeDialog} currentFolderId={currentFolderId}
-                            editServerId={editServerId}/>
+                          editServerId={editServerId} />
+            <ProxmoxDialog open={proxmoxDialogOpen} onClose={() => setProxmoxDialogOpen(false)}
+                           currentFolderId={currentFolderId}
+                           editServerId={editServerId} />
             <ServerList setServerDialogOpen={() => setServerDialogOpen(true)} connectToServer={connectToServer}
-                        connectToPVEServer={connectToPVEServer}
-                        setCurrentFolderId={setCurrentFolderId} setEditServerId={setEditServerId}/>
-            {activeSessions.length === 0 &&<div className="welcome-area">
+                        connectToPVEServer={connectToPVEServer} setProxmoxDialogOpen={() => setProxmoxDialogOpen(true)}
+                        setCurrentFolderId={setCurrentFolderId} setEditServerId={setEditServerId} />
+            {activeSessions.length === 0 && <div className="welcome-area">
                 <div className="area-left">
                     <h1>Hi, <span>{user?.firstName || "User"} {user?.lastName || "name"}</span>!</h1>
                     <p>Welcome to Nexterm. The open-source server manager for SSH, VNC and RDP.</p>
@@ -68,8 +76,9 @@ export const Servers = () => {
                 </div>
                 <img src={WelcomeImage} alt="Welcome" />
             </div>}
-            {activeSessions.length > 0 && <ViewContainer activeSessions={activeSessions} disconnectFromServer={disconnectFromServer}
-                                              activeSessionId={activeSessionId} setActiveSessionId={setActiveSessionId} />}
+            {activeSessions.length > 0 &&
+                <ViewContainer activeSessions={activeSessions} disconnectFromServer={disconnectFromServer}
+                               activeSessionId={activeSessionId} setActiveSessionId={setActiveSessionId} />}
         </div>
-    )
-}
+    );
+};
