342cac182a158f005b4862e339e65c0e1baed5cc
Mathias Wagner <germannewsmaker@gmail.com>
2024-09-01 17:08:33 +0200

    Create pve.js middleware

---
+++ server/middlewares/pve.js
@@ -0,0 +1,44 @@
+const Session = require("../models/Session");
+const Account = require("../models/Account");
+const PVEServer = require("../models/PVEServer");
+
+module.exports = async (ws, req) => {
+    const authHeader = req.query["sessionToken"];
+    const serverId = req.query["serverId"];
+    let containerId = req.query["containerId"];
+
+    if (!authHeader) {
+        ws.close(4001, "You need to provide the token in the 'sessionToken' parameter");
+        return;
+    }
+
+    if (!serverId) {
+        ws.close(4002, "You need to provide the serverId in the 'serverId' parameter");
+        return;
+    }
+
+    if (!containerId) {
+        containerId = "0";
+    }
+
+    req.session = await Session.findOne({ where: { token: authHeader } });
+    if (req.session === null) {
+        ws.close(4003, "The token is not valid");
+        return;
+    }
+
+    await Session.update({ lastActivity: new Date() }, { where: { id: req.session.id } });
+
+    req.user = await Account.findByPk(req.session.accountId);
+    if (req.user === null) {
+        ws.close(4004, "The token is not valid");
+        return;
+    }
+
+    const server = await PVEServer.findByPk(serverId);
+    if (server === null) return;
+
+    console.log("Authorized connection to pve server " + server.ip + " with container " + containerId);
+
+    return { server, containerId };
+}
