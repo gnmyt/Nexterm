c03562415d5362f6a0728c51c89604589bb96d3b
Mathias Wagner <germannewsmaker@gmail.com>
2024-09-01 16:34:25 +0200

    Update ServerTabs.jsx to support PVE

--- client/src/pages/Servers/components/ViewContainer/components/ServerTabs/ServerTabs.jsx
+++ client/src/pages/Servers/components/ViewContainer/components/ServerTabs/ServerTabs.jsx
@@ -2,23 +2,25 @@ import { ServerContext } from "@/common/contexts/ServerContext.jsx";
 import { useContext } from "react";
 import Icon from "@mdi/react";
 import { loadIcon } from "@/pages/Servers/components/ServerList/components/ServerObject/ServerObject.jsx";
+import { getIconByType} from "@/pages/Servers/components/ServerList/components/PVEObject/PVEObject.jsx";
 import { mdiClose, mdiViewSplitVertical } from "@mdi/js";
 import "./styles.sass";
 
 export const ServerTabs = ({activeSessions, setActiveSessionId, activeSessionId, disconnectFromServer}) => {
 
-    const {getServerById} = useContext(ServerContext);
+    const {getServerById, getPVEContainerById} = useContext(ServerContext);
 
     return (
         <div className="server-tabs">
             <Icon path={mdiViewSplitVertical} onClick={() => alert("Not implemented yet")} />
             <div className="tabs">
                 {activeSessions.map(session => {
-                    const server = getServerById(session.server);
+                    let server = session.containerId === undefined ? getServerById(session.server) : getPVEContainerById(session.server, session.containerId);
+
                     return (
                         <div key={session.id} className={"server-tab" + (session.id === activeSessionId ? " server-tab-active" : "")}
                                 onClick={() => setActiveSessionId(session.id)}>
-                            <Icon path={loadIcon(server.icon)} />
+                            <Icon path={server.icon ? loadIcon(server.icon) : getIconByType(server.type)} />
                             <h2>{server.name}</h2>
                             <Icon path={mdiClose} onClick={(e) => {
                                 e.stopPropagation();
