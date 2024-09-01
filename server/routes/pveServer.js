a4c479792befba0bbdc14863b6e9d7a3106f1cb4
Mathias Wagner <germannewsmaker@gmail.com>
2024-09-01 19:00:17 +0200

    Add new routes to pveServer.js

--- server/routes/pveServer.js
+++ server/routes/pveServer.js
@@ -1,7 +1,8 @@
 const { Router } = require("express");
 const { validateSchema } = require("../utils/schema");
-const { getPVEServer, createPVEServer, deletePVEServer, editPVEServer } = require("../controllers/pveServer");
+const { getPVEServer, createPVEServer, deletePVEServer, editPVEServer, getPVEServerUnsafe } = require("../controllers/pveServer");
 const { createPVEServerValidation, updatePVEServerValidation } = require("../validations/pveServer");
+const { startPVEServer, shutdownPVEServer, stopPVEServer } = require("../controllers/pve");
 
 const app = Router();
 
@@ -35,6 +36,58 @@ app.patch("/:serverId", async (req, res) => {
     if (server?.code) return res.json(server);
 
     res.json({ message: "Server got successfully edited" });
+});
+
+app.post("/:type/:pveId/:vmId/start", async (req, res) => {
+    const server = await getPVEServerUnsafe(req.user.id, req.params.pveId);
+    if (server?.code) return res.json(server);
+
+
+    if (req.params.type !== "qemu" && req.params.type !== "lxc")
+        return res.json({ code: 400, message: "Server is not a PVE server" });
+
+    try {
+        const status = await startPVEServer(server, req.params.vmId, req.params.type);
+        if (status?.code) return res.json(status);
+    } catch (e) {
+        return res.json({ code: 500, message: "Server could not get started" });
+    }
+
+    res.json({ message: "Server got successfully started" });
+});
+
+app.post("/:type/:pveId/:vmId/stop", async (req, res) => {
+    const server = await getPVEServerUnsafe(req.user.id, req.params.pveId);
+    if (server?.code) return res.json(server);
+
+    if (req.params.type !== "qemu" && req.params.type !== "lxc")
+        return res.json({ code: 400, message: "Server is not a PVE server" });
+
+    try {
+        const status = await stopPVEServer(server, req.params.vmId, req.params.type);
+        if (status?.code) return res.json(status);
+    } catch (e) {
+        return res.json({ code: 500, message: "Server could not get stopped" });
+    }
+
+    res.json({ message: "Server got successfully stopped" });
+});
+
+app.post("/:type/:pveId/:vmId/shutdown", async (req, res) => {
+    const server = await getPVEServerUnsafe(req.user.id, req.params.pveId);
+    if (server?.code) return res.json(server);
+
+    if (req.params.type !== "qemu" && req.params.type !== "lxc")
+        return res.json({ code: 400, message: "Server is not a PVE server" });
+
+    try {
+        const status = await shutdownPVEServer(server, req.params.vmId, req.params.type);
+        if (status?.code) return res.json(status);
+    } catch (e) {
+        return res.json({ code: 500, message: "Server could not get shutdown" });
+    }
+
+    res.json({ message: "Server got successfully shutdown" });
 });
 
 module.exports = app;
