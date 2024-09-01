bf5dc9d5dace8fef21ed52247cfe0f49e9020c8f
Mathias Wagner <germannewsmaker@gmail.com>
2024-09-01 19:20:20 +0200

    Implement proxmox in context menu

--- client/src/pages/Servers/components/ServerList/components/ContextMenu/ContextMenu.jsx
+++ client/src/pages/Servers/components/ServerList/components/ContextMenu/ContextMenu.jsx
@@ -5,22 +5,23 @@ import {
     mdiFolderPlus,
     mdiFolderRemove,
     mdiFormTextbox,
-    mdiPencil,
+    mdiPencil, mdiPower,
     mdiServerMinus,
-    mdiServerPlus,
+    mdiServerPlus, mdiStop,
 } from "@mdi/js";
-import { deleteRequest, putRequest } from "@/common/utils/RequestUtil.js";
+import { deleteRequest, postRequest, putRequest } from "@/common/utils/RequestUtil.js";
 import { ServerContext } from "@/common/contexts/ServerContext.jsx";
 import { useContext } from "react";
 
 export const ContextMenu = ({
                                 position, id, type, setRenameStateId, setServerDialogOpen, setCurrentFolderId,
-                                setEditServerId, connectToServer,
+                                setEditServerId, connectToServer, connectToPVEServer,
                             }) => {
 
-    const { loadServers, getServerById } = useContext(ServerContext);
+    const { loadServers, getServerById, getPVEServerById, getPVEContainerById } = useContext(ServerContext);
 
-    const server = getServerById(id);
+    const server = type === "server-object" ? getServerById(id) : type === "pve-object"
+        ? getPVEServerById(id.split("-")[1]) : getPVEContainerById(id.split("-")[1], id.split("-")[2]);
 
     const createFolder = () => {
         putRequest("folders", {
@@ -41,6 +42,11 @@ export const ContextMenu = ({
     };
 
     const connect = () => {
+        if (type === "pve-entry") {
+            connectToPVEServer(id.split("-")[1], id.split("-")[2]);
+            return;
+        }
+
         connectToServer(server?.id, server?.identities[0]);
     };
 
@@ -49,13 +55,23 @@ export const ContextMenu = ({
         setServerDialogOpen();
     };
 
+    const postPVEAction = (type) => {
+        const serverType = server?.type === "pve-qemu" ? "qemu" : "lxc";
+        postRequest("pve-servers/" + serverType + "/" + id.split("-")[1] + "/" + server?.id + "/" + type)
+            .then(loadServers);
+    };
+
+    const deletePVEServer = () => {
+        deleteRequest("pve-servers/" + id.split("-")[1]).then(loadServers);
+    }
 
     return (
         <div className="context-menu" style={{ top: position.y, left: position.x }}>
-            {type !== "server-object" && <div className="context-item" onClick={createFolder}>
-                <Icon path={mdiFolderPlus} />
-                <p>Create Folder</p>
-            </div>}
+            {type !== "server-object" && type !== "pve-object" && type !== "pve-entry" &&
+                <div className="context-item" onClick={createFolder}>
+                    <Icon path={mdiFolderPlus} />
+                    <p>Create Folder</p>
+                </div>}
             {type === "folder-object" && <>
                 <div className="context-item" onClick={deleteFolder}>
                     <Icon path={mdiFolderRemove} />
@@ -82,6 +98,44 @@ export const ContextMenu = ({
                 <div className="context-item" onClick={deleteServer}>
                     <Icon path={mdiServerMinus} />
                     <p>Delete Server</p>
+                </div>
+            </>}
+
+            {type === "pve-object" && <>
+                <div className="context-item" onClick={editServer}>
+                    <Icon path={mdiPencil} />
+                    <p>Edit PVE</p>
+                </div>
+                <div className="context-item" onClick={deletePVEServer}>
+                    <Icon path={mdiServerMinus} />
+                    <p>Delete PVE</p>
+                </div>
+            </>
+            }
+
+            {server?.status === "running" && <>
+                <div className="context-item" onClick={connect}>
+                    <Icon path={mdiConnection} />
+                    <p>Connect</p>
+                </div>
+            </>}
+
+            {server?.status === "running" && server.type === "pve-qemu" && <>
+                <div className="context-item" onClick={() => postPVEAction("shutdown")}>
+                    <Icon path={mdiPower} />
+                    <p>Shutdown</p>
+                </div>
+
+                <div className="context-item" onClick={() => postPVEAction("stop")}>
+                    <Icon path={mdiStop} />
+                    <p>Stop</p>
+                </div>
+            </>}
+
+            {server?.status === "stopped" && <>
+                <div className="context-item" onClick={() => postPVEAction("start")}>
+                    <Icon path={mdiPower} />
+                    <p>Start</p>
                 </div>
             </>}
 
