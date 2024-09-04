4c40004f2d3dae9e34bd5b474cc8d5fbaa7bc19a
Mathias Wagner <germannewsmaker@gmail.com>
2024-09-04 10:19:25 +0200

    Hotfix guacamoleStarter.js

--- server/utils/guacamoleStarter.js
+++ server/utils/guacamoleStarter.js
@@ -3,8 +3,9 @@ const net = require("net");
 const { GUACD_TOKEN } = require("../index");
 
 let GUACAMOLE_LITE_PORT = 58390;
+let GUACAMOLE_LITE_HOST = "::";
 
-const checkFreePort = async (port) => {
+const checkFreePort = async (port, host) => {
     return new Promise((resolve) => {
         const server = net.createServer();
         server.once("error", () => resolve(false));
@@ -12,14 +13,21 @@ const checkFreePort = async (port) => {
             server.close();
             resolve(true);
         });
-        server.listen(port, "::");
+        server.listen(port, host);
     });
 };
 
 const getFreePort = async () => {
     for (let i = 0; i < 10; i++) {
         const port = Math.floor(Math.random() * 10000) + 50000;
-        if (await checkFreePort(port)) {
+
+        if (await checkFreePort(port, "::")) {
+            GUACAMOLE_LITE_HOST = "::";
+            return port;
+        }
+
+        if (await checkFreePort(port, "0.0.0.0")) {
+            GUACAMOLE_LITE_HOST = "0.0.0.0";
             return port;
         }
     }
@@ -29,9 +37,9 @@ const getFreePort = async () => {
 module.exports.startGuacamole = async () => {
     GUACAMOLE_LITE_PORT = await getFreePort();
 
-    console.log("Starting Guacamole on port " + GUACAMOLE_LITE_PORT);
+    console.log(`Starting Guacamole on port ${GUACAMOLE_LITE_PORT} using host ${GUACAMOLE_LITE_HOST}`);
 
-    new GuacamoleLite({ port: GUACAMOLE_LITE_PORT, host: "::" }, { port: 4822 }, {
+    new GuacamoleLite({ port: GUACAMOLE_LITE_PORT, host: GUACAMOLE_LITE_HOST }, { port: 4822 }, {
         crypt: { cypher: "AES-256-CBC", key: GUACD_TOKEN },
         log: { level: 0 },
     }).on("error", () => {
