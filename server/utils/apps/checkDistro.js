31fc6b3ed94f29aa115fe60727f6f8d181f8a683
Mathias Wagner <germannewsmaker@gmail.com>
2024-09-05 14:45:49 +0200

    Create checkDistro.js step

---
+++ server/utils/apps/checkDistro.js
@@ -0,0 +1,22 @@
+module.exports.checkDistro = (ssh, ws) => {
+    return new Promise((resolve, reject) => {
+        ssh.exec("lsb_release -i -s && lsb_release -r -s", (err, stream) => {
+            if (err) return reject(new Error("Failed to check distro"));
+
+            let data = "";
+
+            stream.on("close", () => {
+                const [distro, version] = data.split("\n");
+                console.log(distro, version);
+                ws.send(`\x021${distro},${version}`);
+                resolve();
+            });
+
+            stream.on("data", newData => {
+                data += newData.toString();
+            });
+
+            stream.stderr.on("data", () => reject(new Error("Failed to check distro")));
+        });
+    });
+}
