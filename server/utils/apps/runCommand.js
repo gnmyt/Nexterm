aa1acf360ec37aa29715776202488e53beec7e4a
Mathias Wagner <germannewsmaker@gmail.com>
2024-09-05 14:51:54 +0200

    Create runCommand.js step

---
+++ server/utils/apps/runCommand.js
@@ -0,0 +1,19 @@
+module.exports.runPreInstallCommand = (ssh, ws, preInstallCommand) => {
+    return new Promise((resolve, reject) => {
+        ssh.exec(preInstallCommand, (err) => {
+            if (err) return reject(new Error("Failed to run pre-install command"));
+            ws.send("\x024,Pre-install command completed");
+            resolve();
+        });
+    });
+}
+
+module.exports.runPostInstallCommand = (ssh, ws, postInstallCommand) => {
+    return new Promise((resolve, reject) => {
+        ssh.exec(postInstallCommand, (err) => {
+            if (err) return reject(new Error("Failed to run post-install command"));
+            ws.send("\x02\x25,Post-install command completed");
+            resolve();
+        });
+    });
+}
