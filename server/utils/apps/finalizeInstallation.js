3857db2734951f30ecc67133ec588057b13e5497
Mathias Wagner <germannewsmaker@gmail.com>
2024-09-05 18:39:07 +0200

    Update finalizeInstallation.js

--- server/utils/apps/finalizeInstallation.js
+++ server/utils/apps/finalizeInstallation.js
@@ -2,7 +2,7 @@ module.exports.finalizeInstallation = (ssh, ws) => {
     return new Promise((resolve, reject) => {
         ssh.exec("echo todo", (err, stream) => {
             if (err) return reject(new Error("Failed to finalize installation"));
-            ws.send("\x02\x26,Installation finalized");
+            ws.send("\x027,Installation finalized");
             resolve();
         });
     });
