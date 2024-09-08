af862e4bae09d1cfdc6527498ce2490cf1ea2384
Mathias Wagner <germannewsmaker@gmail.com>
2024-09-08 13:31:28 +0200

    Add disabled parameter to Button.jsx component

--- client/src/common/components/Button/Button.jsx
+++ client/src/common/components/Button/Button.jsx
@@ -1,9 +1,9 @@
 import "./styles.sass";
 import Icon from "@mdi/react";
 
-export const Button = ({onClick, text, icon}) => {
+export const Button = ({onClick, text, icon, disabled}) => {
     return (
-        <button className="btn" onClick={onClick}>
+        <button className="btn" onClick={onClick} disabled={disabled}>
             {icon ? <Icon path={icon} /> : null}
             <h3>{text}</h3>
         </button>
