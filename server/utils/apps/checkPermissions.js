7e38d52538ee85056394d6d7cb597edc79e3d30e
Mathias Wagner <germannewsmaker@gmail.com>
2024-09-08 15:28:55 +0200

    Fix bug in checkPermissions.js

--- server/utils/apps/checkPermissions.js
+++ server/utils/apps/checkPermissions.js
@@ -1,18 +1,32 @@
 const checkSudoPermissions = (ssh, ws, identity) => {
     return new Promise((resolve, reject) => {
-        ssh.exec("sudo -n true", (err) => {
-            if (!err) {
-                ws.send(`\x022,Sudo access granted`);
-                return resolve("sudo ");
-            }
+        ssh.exec("sudo -n true", (err, stream) => {
+            if (err) return reject(new Error("Failed to check sudo permissions"));
 
-            ssh.exec(`echo ${identity.password} | sudo -S true`, (err) => {
-                if (err) {
-                    return reject(new Error("Failed to get sudo permissions"));
+            stream.on("data", () => {});
+
+            stream.on("close", (code) => {
+                if (code === 0) {
+                    ws.send(`\x022,Sudo access granted`);
+                    return resolve("sudo ");
                 }
 
-                ws.send(`\x022,Sudo access granted`);
-                resolve(`echo ${identity.password} | sudo -S `);
+                ssh.exec(`echo ${identity.password} | sudo -S true`, (err, stream) => {
+                    if (err) return reject(new Error("Failed to check sudo permissions"));
+
+                    stream.on("data", () => {});
+
+                    stream.on("close", (code) => {
+                        if (code === 0) {
+                            ws.send(`\x022,Sudo access granted`);
+                            return resolve(`echo ${identity.password} | sudo -S `);
+                        }
+
+                        ws.send(`\x021,Sudo access denied`);
+                        reject(new Error("Sudo access denied"));
+
+                    });
+                });
             });
         });
     });
