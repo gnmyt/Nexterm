af667d5f33f649fe5624d5d99f8ffb7082d33b7e
Mathias Wagner <germannewsmaker@gmail.com>
2024-09-08 11:15:13 +0200

    Implement icons into the Button.jsx component

--- client/src/common/components/Button/Button.jsx
+++ client/src/common/components/Button/Button.jsx
@@ -1,8 +1,10 @@
 import "./styles.sass";
+import Icon from "@mdi/react";
 
-export const Button = ({onClick, text}) => {
+export const Button = ({onClick, text, icon}) => {
     return (
         <button className="btn" onClick={onClick}>
+            {icon ? <Icon path={icon} /> : null}
             <h3>{text}</h3>
         </button>
     );
