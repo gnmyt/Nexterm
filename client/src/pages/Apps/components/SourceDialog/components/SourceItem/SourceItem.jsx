6aa8cd15807e3ab32ebaf6a17e13856409035bfe
Mathias Wagner <germannewsmaker@gmail.com>
2024-09-11 17:56:58 +0200

    Implement logic into the SourceItem component

--- client/src/pages/Apps/components/SourceDialog/components/SourceItem/SourceItem.jsx
+++ client/src/pages/Apps/components/SourceDialog/components/SourceItem/SourceItem.jsx
@@ -1,45 +1,75 @@
 import Icon from "@mdi/react";
-import { mdiBook, mdiChevronDown, mdiChevronUp, mdiFormTextbox, mdiTrashCan, mdiWeb } from "@mdi/js";
+import { mdiBook, mdiChevronDown, mdiChevronUp, mdiContentSave, mdiFormTextbox, mdiTrashCan, mdiWeb } from "@mdi/js";
 import IconInput from "@/common/components/IconInput";
 import "./styles.sass";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { deleteRequest, patchRequest, putRequest } from "@/common/utils/RequestUtil.js";
 
-export const SourceItem = ({ name, url, isNew }) => {
+export const SourceItem = ({ name, url, isNew, fetchSources, setCreateNew }) => {
 
     const [isOpen, setIsOpen] = useState(isNew);
 
-    const [currentName, setCurrentName] = useState(name);
-    const [currentUrl, setCurrentUrl] = useState(url);
+    const [error, setError] = useState("");
+
+    const [unsavedChanges, setUnsavedChanges] = useState(true);
+
+    const [currentName, setCurrentName] = useState(name || "");
+    const [currentUrl, setCurrentUrl] = useState(url || "");
 
     const deleteSelf = () => {
-        console.log("Delete source");
+        deleteRequest("apps/sources/" + encodeURIComponent(name)).then(() => fetchSources()).catch(err => setError(err.message));
+    };
+
+    const updateChanges = () => {
+        patchRequest("apps/sources/" + encodeURIComponent(name), { url: currentUrl }).then(() => {
+            fetchSources();
+            setUnsavedChanges(false);
+        }).catch(err => setError(err.message));
     }
+
+    const createSource = () => {
+        putRequest("apps/sources", { name: currentName, url: currentUrl }).then(() => {
+            fetchSources();
+            setCreateNew(false);
+        }).catch(err => setError(err.message));
+    };
+
+    useEffect(() => {
+        setUnsavedChanges(currentName !== name || currentUrl !== url);
+
+        setError("");
+    }, [currentName, currentUrl]);
 
     return (
         <div className="source-item">
-            <div className={"source-header" + (name === "official" ? " header-official" : "")}
-                 onClick={() => name === "official" ? null : setIsOpen(!isOpen)}>
+            <div className={"source-header" + (name === "official" ? " header-official" : "")}>
                 <div className="source-info">
                     <Icon path={mdiBook} />
-                    <h2>{name === "official" ? "Official" : isNew ? "New source" : name}</h2>
+                    <h2>{name === "official" ? "Official" : isNew && currentName === "" ? "New source" : currentName}</h2>
                     {name === "official" &&
                         <span>@ {url?.toString().replace("https://", "").replace("http://", "").split("/")[0]}</span>}
                 </div>
-                <div class="action-area">
-                    {name !== "official" && <Icon path={mdiTrashCan} onClick={() => console.log("Delete source")} />}
-                    {name !== "official" && <Icon path={isOpen ? mdiChevronUp : mdiChevronDown} />}
+                <div className="action-area">
+                    {name !== "official" && !isNew &&
+                        <Icon path={mdiTrashCan} onClick={() => deleteSelf()} className="action-delete" />}
+                    {name !== "official" &&
+                        <Icon path={isOpen ? (unsavedChanges ? mdiContentSave : mdiChevronUp) : mdiChevronDown}
+                              onClick={() => unsavedChanges ? (isNew ? createSource() : updateChanges()) : setIsOpen(!isOpen)}
+                                className="action-open" />}
                 </div>
             </div>
 
             {isOpen && <div className="edit-area">
-                <div className="edit-row">
+                {error && <div className="error">{error}</div>}
+
+                {isNew && <div className="edit-row">
                     <h3>Name</h3>
-                    <IconInput icon={mdiFormTextbox} value={currentName} onChange={setCurrentName}
+                    <IconInput icon={mdiFormTextbox} value={currentName} setValue={setCurrentName}
                                placeholder="Source name" />
-                </div>
+                </div>}
                 <div className="edit-row">
                     <h3>URL</h3>
-                    <IconInput icon={mdiWeb} value={currentUrl} onChange={setCurrentUrl}
+                    <IconInput icon={mdiWeb} value={currentUrl} setValue={setCurrentUrl}
                                placeholder="Source URL" />
                 </div>
             </div>}
