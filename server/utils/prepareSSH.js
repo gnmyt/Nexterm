bcafee5d03b10d0571c5b8f95f3b8ffe13ff2111
Mathias Wagner <germannewsmaker@gmail.com>
2024-09-11 16:48:10 +0200

    Resolve merge conflicts

--- server/utils/prepareSSH.js
+++ server/utils/prepareSSH.js
@@ -1,45 +1,51 @@
 const sshd = require("ssh2");
 
 module.exports = (server, identity, ws) => {
-    let options;
-    if (identity.type === "password") {
-        options = {
-            host: server.ip,
-            port: server.port,
-            username: identity.username,
-            password: identity.password,
-        };
-    } else if (identity.type === "ssh") {
-        options = {
-            host: server.ip,
-            port: server.port,
-            username: identity.username,
-            privateKey: identity.sshKey,
-            passphrase: identity.passphrase,
-        };
-    }
+    const options = {
+        host: server.ip,
+        port: server.port,
+        username: identity.username,
+        tryKeyboard: true,
+        ...(identity.type === "password" ? { password: identity.password } : { privateKey: identity.sshKey, passphrase: identity.passphrase })
+    };
 
     let ssh = new sshd.Client();
+
+    ssh.on("error", (error) => {
+        if(error.level === "client-timeout") {
+            ws.close(4007, "Client Timeout reached");
+        } else {
+            ws.close(4005, error.message);
+        }
+    });
+
+    ssh.on("keyboard-interactive", (name, instructions, lang, prompts, finish) => {
+        ws.send(`\x02${prompts[0].prompt}`);
+
+        ws.on("message", (data) => {
+            if (data.toString().startsWith("\x03")) {
+                const totpCode = data.substring(1);
+                finish([totpCode]);
+            }
+        });
+    });
+
     try {
         ssh.connect(options);
     } catch (err) {
         ws.close(4004, err.message);
     }
 
-    ssh.on("error", (err) => {
-        ws.close(4005, err.message);
-    });
-
     ssh.on("end", () => {
         ws.close(4006, "Connection closed");
     });
 
-    ssh.on("close", () => {
-        ws.close(4007, "Connection closed");
+    ssh.on("exit", () => {
+        ws.close(4006, "Connection exited");
     });
 
-    ssh.on("keyboard-interactive", (name, instructions, instructionsLang, prompts, finish) => {
-        finish([identity.password]);
+    ssh.on("close", () => {
+        ws.close(4007, "Connection closed");
     });
 
     console.log("Authorized connection to server " + server.ip + " with identity " + identity.name);
