29321db55e18878b46e18bbe4b5f0494acde15b2
Mathias Wagner <germannewsmaker@gmail.com>
2024-09-11 16:59:31 +0200

    Migrate checkDistro.js to cat /etc/os-release since lsb_release may not be available

--- server/utils/apps/checkDistro.js
+++ server/utils/apps/checkDistro.js
@@ -1,14 +1,30 @@
 module.exports.checkDistro = (ssh, ws) => {
     return new Promise((resolve, reject) => {
-        ssh.exec("lsb_release -i -s && lsb_release -r -s", (err, stream) => {
+        ssh.exec("cat /etc/os-release", (err, stream) => {
             if (err) return reject(new Error("Failed to check distro"));
 
             let data = "";
 
             stream.on("close", () => {
-                const [distro, version] = data.split("\n");
-                ws.send(`\x021,${distro},${version}`);
-                resolve();
+                let distro = null;
+                let version = null;
+
+                data.split("\n").forEach(line => {
+                    if (line.startsWith("ID=")) {
+                        distro = line.split("=")[1].replace(/"/g, "")
+                            .replace(/./, c => c.toUpperCase());
+                    }
+                    if (line.startsWith("VERSION_ID=")) {
+                        version = line.split("=")[1].replace(/"/g, "");
+                    }
+                });
+
+                if (distro && version) {
+                    ws.send(`\x021,${distro},${version}`);
+                    resolve();
+                } else {
+                    reject(new Error("Failed to parse distro information"));
+                }
             });
 
             stream.on("data", newData => {
