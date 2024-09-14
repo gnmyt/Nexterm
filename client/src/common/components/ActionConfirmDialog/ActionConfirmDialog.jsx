0648ec0f7bc31eaa5ecd018d02f7912b6c2c9830
Mathias Wagner <germannewsmaker@gmail.com>
2024-09-14 14:00:07 +0200

    Create ActionConfirmDialog component

---
+++ client/src/common/components/ActionConfirmDialog/ActionConfirmDialog.jsx
@@ -0,0 +1,35 @@
+import "./styles.sass";
+import { DialogProvider } from "@/common/components/Dialog";
+import Button from "@/common/components/Button/index.js";
+
+export const ActionConfirmDialog = ({open, setOpen, onConfirm, onCancel, text}) => {
+
+    const cancel = () => {
+        setOpen(false);
+
+        if (onCancel) {
+            onCancel();
+        }
+    }
+
+    const confirm = () => {
+        setOpen(false);
+
+        if (onConfirm) {
+            onConfirm();
+        }
+    }
+
+    return (
+        <DialogProvider onClose={() => setOpen(false)} open={open}>
+            <div className="confirm-dialog">
+                <h2>Are you sure?</h2>
+                <p>{text ? text : "This action cannot be undone."}</p>
+                <div className="btn-area">
+                    <Button onClick={cancel} type="secondary" text="Cancel" />
+                    <Button onClick={confirm} type="primary" text="Confirm" />
+                </div>
+            </div>
+        </DialogProvider>
+    )
+}
