353b676baf8adf165fbbf308b7d9c55cd6b7fad7
Mathias Wagner <germannewsmaker@gmail.com>
2024-09-14 15:04:19 +0200

    Integrate overrideToken into UserContext.jsx

--- client/src/common/contexts/UserContext.jsx
+++ client/src/common/contexts/UserContext.jsx
@@ -1,12 +1,13 @@
 import { createContext, useEffect, useState } from "react";
 import LoginDialog from "@/common/components/LoginDialog";
-import { getRequest } from "@/common/utils/RequestUtil.js";
+import { getRequest, postRequest } from "@/common/utils/RequestUtil.js";
 
 export const UserContext = createContext({});
 
 export const UserProvider = ({ children }) => {
 
-    const [sessionToken, setSessionToken] = useState(localStorage.getItem("sessionToken"));
+    const [sessionToken, setSessionToken] = useState(localStorage.getItem("overrideToken")
+        || localStorage.getItem("sessionToken"));
     const [firstTimeSetup, setFirstTimeSetup] = useState(false);
     const [user, setUser] = useState(null);
 
@@ -15,7 +16,7 @@ export const UserProvider = ({ children }) => {
         localStorage.setItem("sessionToken", sessionToken);
 
         login();
-    }
+    };
 
     const checkFirstTimeSetup = async () => {
         try {
@@ -24,7 +25,7 @@ export const UserProvider = ({ children }) => {
         } catch (error) {
             console.error(error);
         }
-    }
+    };
 
     const login = async () => {
         try {
@@ -36,16 +37,33 @@ export const UserProvider = ({ children }) => {
                 localStorage.removeItem("sessionToken");
             }
         }
-    }
+    };
+
+    const logout = async () => {
+        await postRequest("auth/logout", { token: sessionToken });
+
+        if (localStorage.getItem("overrideToken")) {
+            localStorage.removeItem("overrideToken");
+            setSessionToken(localStorage.getItem("sessionToken"));
+        }
+
+        login();
+    };
+
+    const overrideToken = (token) => {
+        localStorage.setItem("overrideToken", token);
+        setSessionToken(token);
+        login();
+    };
 
     useEffect(() => {
         sessionToken ? login() : checkFirstTimeSetup();
     }, []);
 
     return (
-        <UserContext.Provider value={{updateSessionToken, user, sessionToken, firstTimeSetup, login}}>
+        <UserContext.Provider value={{ updateSessionToken, user, sessionToken, firstTimeSetup, login, logout, overrideToken }}>
             <LoginDialog open={!sessionToken} />
             {children}
         </UserContext.Provider>
     );
-}
+};
