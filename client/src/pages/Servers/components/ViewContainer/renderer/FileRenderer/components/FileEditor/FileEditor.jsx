9b17f68c9e15729a626008f60bfbdce77d11c76e
Mathias Wagner <germannewsmaker@gmail.com>
2024-09-15 15:34:46 +0200

    Create FileEditor component

---
+++ client/src/pages/Servers/components/ViewContainer/renderer/FileRenderer/components/FileEditor/FileEditor.jsx
@@ -0,0 +1,89 @@
+import CodeMirror from "@uiw/react-codemirror";
+import { githubDark } from "@uiw/codemirror-theme-github";
+import { useContext, useEffect, useState } from "react";
+import "./styles.sass";
+import { UserContext } from "@/common/contexts/UserContext.jsx";
+import { downloadRequest } from "@/common/utils/RequestUtil.js";
+import Icon from "@mdi/react";
+import { mdiClose, mdiContentSave, mdiTextBox } from "@mdi/js";
+import { ActionConfirmDialog } from "@/common/components/ActionConfirmDialog/ActionConfirmDialog.jsx";
+
+export const FileEditor = ({ currentFile, serverId, identityId, setCurrentFile, sendOperation }) => {
+    const [fileContent, setFileContent] = useState("");
+    const [fileContentChanged, setFileContentChanged] = useState(false);
+
+    const [unsavedChangesDialog, setUnsavedChangesDialog] = useState(false);
+
+    const { sessionToken } = useContext(UserContext);
+
+    useEffect(() => {
+        if (currentFile === null) return setFileContent(null);
+        const url = `/api/servers/sftp-download?serverId=${serverId}&identityId=${identityId}&path=${currentFile}&sessionToken=${sessionToken}`;
+
+        downloadRequest(url).then((res) => {
+            const reader = new FileReader();
+            reader.onload = () => {
+                setFileContent(reader.result);
+            };
+            reader.readAsText(res);
+        });
+    }, [currentFile]);
+
+    useEffect(() => {
+        return () => setFileContent(null);
+    }, []);
+
+    const saveFile = () => {
+        sendOperation(0x2, { path: currentFile });
+
+        const chunks = [];
+        let chunkIndex = 0;
+        const chunkSize = 1024;
+
+        while (chunkIndex < fileContent.length) {
+            chunks.push(fileContent.slice(chunkIndex, chunkIndex + chunkSize));
+            chunkIndex += chunkSize;
+        }
+
+        for (let i = 0; i < chunks.length; i++) {
+            sendOperation(0x3, { chunk: btoa(chunks[i]) });
+        }
+
+        sendOperation(0x4);
+
+        setFileContentChanged(false);
+    };
+
+    const closeFile = () => {
+        if (fileContentChanged) {
+            setUnsavedChangesDialog(true);
+        } else {
+            setCurrentFile(null);
+        }
+    }
+
+    const updateContent = (value) => {
+        setFileContentChanged(true);
+        setFileContent(value);
+    };
+
+    return (
+        <div className="file-editor">
+            <ActionConfirmDialog text="You have unsaved changes. Are you sure you want to close this file?"
+                                 onConfirm={() => setCurrentFile(null)}
+                                 open={unsavedChangesDialog} setOpen={setUnsavedChangesDialog} />
+            <div className="file-header">
+                <div className="file-name">
+                    <Icon path={mdiTextBox} />
+                    <h2>{currentFile}</h2>
+                </div>
+                <div className="file-actions">
+                    <Icon path={mdiContentSave} onClick={() => saveFile()} className={fileContentChanged ? "" : " icon-disabled"} />
+                    <Icon path={mdiClose} onClick={() => closeFile()} />
+                </div>
+            </div>
+            <CodeMirror value={fileContent === null ? "Loading..." : fileContent} onChange={updateContent}
+                        theme={githubDark} />
+        </div>
+    );
+};
