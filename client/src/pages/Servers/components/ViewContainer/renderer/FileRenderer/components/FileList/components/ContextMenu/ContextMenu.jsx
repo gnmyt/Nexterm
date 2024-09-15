321559bedf772d4afdc27ceaba86dce487b55563
Mathias Wagner <germannewsmaker@gmail.com>
2024-09-15 16:06:01 +0200

    Add "Edit" item to ContextMenu

--- client/src/pages/Servers/components/ViewContainer/renderer/FileRenderer/components/FileList/components/ContextMenu/ContextMenu.jsx
+++ client/src/pages/Servers/components/ViewContainer/renderer/FileRenderer/components/FileList/components/ContextMenu/ContextMenu.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import Icon from "@mdi/react";
-import { mdiFileDownload, mdiFormTextbox, mdiTrashCan } from "@mdi/js";
+import { mdiFileDownload, mdiFormTextbox, mdiTextBoxEdit, mdiTrashCan } from "@mdi/js";
 import RenameItemDialog from "../RenameItemDialog";
+import { ActionConfirmDialog } from "@/common/components/ActionConfirmDialog/ActionConfirmDialog.jsx";
 
-export const ContextMenu = ({ menuPosition, selectedItem, closeContextMenu, sendOperation, path, downloadFile }) => {
+export const ContextMenu = ({ menuPosition, selectedItem, closeContextMenu, sendOperation, path, downloadFile, setCurrentFile }) => {
 
     const [renameDialogOpen, setRenameDialogOpen] = useState(false);
+    const [bigFileDialogOpen, setBigFileDialogOpen] = useState(false);
 
     const handleDelete = () => {
         sendOperation(selectedItem.type === "folder" ? 0x7 : 0x6, { path: path + "/" + selectedItem.name });
@@ -25,10 +27,21 @@ export const ContextMenu = ({ menuPosition, selectedItem, closeContextMenu, send
         closeContextMenu();
     }
 
+    const openFile = () => {
+        if (selectedItem.type === "file" && selectedItem.size >= 1024 * 1024) {
+            setBigFileDialogOpen(true);
+        } else {
+            setCurrentFile(path + "/" + selectedItem.name);
+        }
+    }
+
     return (
         <>
             <RenameItemDialog open={renameDialogOpen} closeDialog={() => setRenameDialogOpen(false)} renameItem={handleRename}
                                 item={selectedItem}/>
+            <ActionConfirmDialog open={bigFileDialogOpen} setOpen={setBigFileDialogOpen}
+                                 onConfirm={() => setCurrentFile(path + "/" + selectedItem?.name)}
+                                 text={`This file is ${Math.round(selectedItem?.size / 1024 / 1024)} MB large. Are you sure you want to edit it?`}/>
             {menuPosition.x !== 0 && menuPosition.y !== 0 &&
                 <div className="context-menu" style={{ top: `${menuPosition.y}px`, left: `${menuPosition.x}px` }}>
                     <div className="context-item" onClick={() => setRenameDialogOpen(true)}>
@@ -36,10 +49,16 @@ export const ContextMenu = ({ menuPosition, selectedItem, closeContextMenu, send
                         <p>Rename</p>
                     </div>
                     {selectedItem.type === "file" && (
-                        <div className="context-item" onClick={() => handleDownload()}>
-                            <Icon path={mdiFileDownload} />
-                            <p>Download</p>
-                        </div>
+                        <>
+                            <div className="context-item" onClick={() => openFile()}>
+                                <Icon path={mdiTextBoxEdit} />
+                                <p>Edit</p>
+                            </div>
+                            <div className="context-item" onClick={() => handleDownload()}>
+                                <Icon path={mdiFileDownload} />
+                                <p>Download</p>
+                            </div>
+                        </>
                     )}
                     <div className="context-item" onClick={() => handleDelete()}>
                         <Icon path={mdiTrashCan} />
