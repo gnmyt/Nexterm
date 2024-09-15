8734f8c41b822d3628219522f8d2511ee3d4ba90
Mathias Wagner <germannewsmaker@gmail.com>
2024-09-15 14:23:50 +0200

    Integrate RenameItemDialog into ContextMenu

--- client/src/pages/Servers/components/ViewContainer/renderer/FileRenderer/components/FileList/components/ContextMenu/ContextMenu.jsx
+++ client/src/pages/Servers/components/ViewContainer/renderer/FileRenderer/components/FileList/components/ContextMenu/ContextMenu.jsx
@@ -1,9 +1,12 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Icon from "@mdi/react";
 import { mdiFileDownload, mdiFormTextbox, mdiTrashCan } from "@mdi/js";
 import { UserContext } from "@/common/contexts/UserContext.jsx";
+import RenameItemDialog from "../RenameItemDialog";
 
 export const ContextMenu = ({ menuPosition, selectedItem, closeContextMenu, sendOperation, path, serverId, identityId }) => {
+
+    const [renameDialogOpen, setRenameDialogOpen] = useState(false);
 
     const {sessionToken} = useContext(UserContext);
 
@@ -25,26 +28,33 @@ export const ContextMenu = ({ menuPosition, selectedItem, closeContextMenu, send
         closeContextMenu();
     }
 
-    const handleRename = () => {
-        console.log("Rename");
-    };
+    const handleRename = (newName) => {
+        sendOperation(0x8, { path: path + "/" + selectedItem.name, newPath: path + "/" + newName });
+
+        closeContextMenu();
+    }
 
     return (
-        <div className="context-menu" style={{ top: `${menuPosition.y}px`, left: `${menuPosition.x}px` }}>
-            <div className="context-item" onClick={() => handleRename()}>
-                <Icon path={mdiFormTextbox} />
-                <p>Rename</p>
-            </div>
-            {selectedItem.type === "file" && (
-                <div className="context-item" onClick={() => handleDownload()}>
-                    <Icon path={mdiFileDownload} />
-                    <p>Download</p>
-                </div>
-            )}
-            <div className="context-item" onClick={() => handleDelete()}>
-                <Icon path={mdiTrashCan} />
-                <p>Delete</p>
-            </div>
-        </div>
+        <>
+            <RenameItemDialog open={renameDialogOpen} closeDialog={() => setRenameDialogOpen(false)} renameItem={handleRename}
+                                item={selectedItem}/>
+            {menuPosition.x !== 0 && menuPosition.y !== 0 &&
+                <div className="context-menu" style={{ top: `${menuPosition.y}px`, left: `${menuPosition.x}px` }}>
+                    <div className="context-item" onClick={() => setRenameDialogOpen(true)}>
+                        <Icon path={mdiFormTextbox} />
+                        <p>Rename</p>
+                    </div>
+                    {selectedItem.type === "file" && (
+                        <div className="context-item" onClick={() => handleDownload()}>
+                            <Icon path={mdiFileDownload} />
+                            <p>Download</p>
+                        </div>
+                    )}
+                    <div className="context-item" onClick={() => handleDelete()}>
+                        <Icon path={mdiTrashCan} />
+                        <p>Delete</p>
+                    </div>
+                </div>}
+        </>
     );
 };
