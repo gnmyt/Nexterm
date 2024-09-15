85e7dd85b00ad06543d099bfd5d683ba41408975
Mathias Wagner <germannewsmaker@gmail.com>
2024-09-15 14:07:13 +0200

    Update prepareSSH.js to support express routes

--- server/utils/prepareSSH.js
+++ server/utils/prepareSSH.js
@@ -1,6 +1,6 @@
 const sshd = require("ssh2");
 
-module.exports = (server, identity, ws) => {
+module.exports = (server, identity, ws, res) => {
     const options = {
         host: server.ip,
         port: server.port,
@@ -10,45 +10,53 @@ module.exports = (server, identity, ws) => {
     };
 
     let ssh = new sshd.Client();
-
-    ssh.on("error", (error) => {
-        if(error.level === "client-timeout") {
-            ws.close(4007, "Client Timeout reached");
-        } else {
-            ws.close(4005, error.message);
-        }
-    });
-
-    ssh.on("keyboard-interactive", (name, instructions, lang, prompts, finish) => {
-        ws.send(`\x02${prompts[0].prompt}`);
-
-        ws.on("message", (data) => {
-            if (data.toString().startsWith("\x03")) {
-                const totpCode = data.substring(1);
-                finish([totpCode]);
+    if (ws) {
+        ssh.on("error", (error) => {
+            if(error.level === "client-timeout") {
+                ws.close(4007, "Client Timeout reached");
+            } else {
+                ws.close(4005, error.message);
             }
         });
-    });
+
+        ssh.on("keyboard-interactive", (name, instructions, lang, prompts, finish) => {
+            ws.send(`\x02${prompts[0].prompt}`);
+
+            ws.on("message", (data) => {
+                if (data.toString().startsWith("\x03")) {
+                    const totpCode = data.substring(1);
+                    finish([totpCode]);
+                }
+            });
+        });
+    }
 
     try {
         ssh.connect(options);
     } catch (err) {
-        ws.close(4004, err.message);
+        if (ws) ws.close(4004, err.message);
+        if (res) res.status(500).send(err.message);
     }
 
-    ssh.on("end", () => {
-        ws.close(4006, "Connection closed");
-    });
+    if (ws) {
+        ssh.on("end", () => {
+            ws.close(4006, "Connection closed");
+        });
 
-    ssh.on("exit", () => {
-        ws.close(4006, "Connection exited");
-    });
+        ssh.on("exit", () => {
+            ws.close(4006, "Connection exited");
+        });
 
-    ssh.on("close", () => {
-        ws.close(4007, "Connection closed");
-    });
+        ssh.on("close", () => {
+            ws.close(4007, "Connection closed");
+        });
+    }
 
-    console.log("Authorized connection to server " + server.ip + " with identity " + identity.name);
+    if (ws) {
+        console.log("Authorized connection to server " + server.ip + " with identity " + identity.name);
+    } else {
+        console.log("Authorized file download from server " + server.ip + " with identity " + identity.name);
+    }
 
     return ssh;
 }
