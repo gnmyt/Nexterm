7150a130ad79992448d4b5a6e04ac4859f6a2e8a
Mathias Wagner <germannewsmaker@gmail.com>
2024-11-19 18:47:13 +0100

    Add position to Folder.js & Server.js model

--- server/models/Server.js
+++ server/models/Server.js
@@ -10,6 +10,11 @@ module.exports = db.define("servers", {
         type: Sequelize.INTEGER,
         allowNull: false,
     },
+    position: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+        allowNull: false,
+    },
     folderId: {
         type: Sequelize.INTEGER,
         allowNull: false,
