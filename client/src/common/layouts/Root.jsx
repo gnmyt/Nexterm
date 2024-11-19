10fb0ff02a9b9f63ea2d7293c87f2d91c62d4fa9
Mathias Wagner <germannewsmaker@gmail.com>
2024-11-19 18:51:51 +0100

    Integrate react-dnd backend into Root.jsx layout

--- client/src/common/layouts/Root.jsx
+++ client/src/common/layouts/Root.jsx
@@ -3,18 +3,22 @@ import Sidebar from "@/common/components/Sidebar";
 import { UserProvider } from "@/common/contexts/UserContext.jsx";
 import { ServerProvider } from "@/common/contexts/ServerContext.jsx";
 import { IdentityProvider } from "@/common/contexts/IdentityContext.jsx";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 export default () => {
     return (
-        <UserProvider>
-            <ServerProvider>
-                <IdentityProvider>
-                    <div className="content-wrapper">
-                        <Sidebar />
-                        <Outlet />
-                    </div>
-                </IdentityProvider>
-            </ServerProvider>
-        </UserProvider>
-    )
+        <DndProvider backend={HTML5Backend}>
+            <UserProvider>
+                <ServerProvider>
+                    <IdentityProvider>
+                        <div className="content-wrapper">
+                            <Sidebar />
+                            <Outlet />
+                        </div>
+                    </IdentityProvider>
+                </ServerProvider>
+            </UserProvider>
+        </DndProvider>
+    );
 }
