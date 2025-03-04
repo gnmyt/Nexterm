28a41db99bd2d1d2add10d68ef5acd74dd8f1437
Mathias Wagner <germannewsmaker@gmail.com>
2025-03-04 20:25:51 +0100

    Integrate ToastContext into Root.jsx

--- client/src/common/layouts/Root.jsx
+++ client/src/common/layouts/Root.jsx
@@ -3,6 +3,7 @@ import Sidebar from "@/common/components/Sidebar";
 import { UserProvider } from "@/common/contexts/UserContext.jsx";
 import { ServerProvider } from "@/common/contexts/ServerContext.jsx";
 import { IdentityProvider } from "@/common/contexts/IdentityContext.jsx";
+import { ToastProvider } from "@/common/contexts/ToastContext.jsx";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
@@ -12,10 +13,12 @@ export default () => {
             <UserProvider>
                 <ServerProvider>
                     <IdentityProvider>
-                        <div className="content-wrapper">
-                            <Sidebar />
-                            <Outlet />
-                        </div>
+                        <ToastProvider>
+                            <div className="content-wrapper">
+                                <Sidebar />
+                                <Outlet />
+                            </div>
+                        </ToastProvider>
                     </IdentityProvider>
                 </ServerProvider>
             </UserProvider>
