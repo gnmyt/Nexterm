b5d4006b0f921aa5e4a56ba975dfed59905fac29
Mathias Wagner <germannewsmaker@gmail.com>
2025-03-07 19:33:55 +0100

    Implement ThemeContext in Root.jsx

--- client/src/common/layouts/Root.jsx
+++ client/src/common/layouts/Root.jsx
@@ -4,6 +4,7 @@ import { UserProvider } from "@/common/contexts/UserContext.jsx";
 import { ServerProvider } from "@/common/contexts/ServerContext.jsx";
 import { IdentityProvider } from "@/common/contexts/IdentityContext.jsx";
 import { ToastProvider } from "@/common/contexts/ToastContext.jsx";
+import { ThemeProvider } from "@/common/contexts/ThemeContext.jsx";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
@@ -11,17 +12,18 @@ export default () => {
     return (
         <DndProvider backend={HTML5Backend}>
             <ToastProvider>
-                <UserProvider>
-                    <ServerProvider>
-                        <IdentityProvider>
-
-                            <div className="content-wrapper">
-                                <Sidebar />
-                                <Outlet />
-                            </div>
-                        </IdentityProvider>
-                    </ServerProvider>
-                </UserProvider>
+                <ThemeProvider>
+                    <UserProvider>
+                        <ServerProvider>
+                            <IdentityProvider>
+                                <div className="content-wrapper">
+                                    <Sidebar />
+                                    <Outlet />
+                                </div>
+                            </IdentityProvider>
+                        </ServerProvider>
+                    </UserProvider>
+                </ThemeProvider>
             </ToastProvider>
         </DndProvider>
     );
