21d1acde15ff7aeadbdead57692fed43b02a3892
Mathias Wagner <germannewsmaker@gmail.com>
2025-03-08 00:39:50 +0100

    Integrate snippet.js route into server index.js

--- server/index.js
+++ server/index.js
@@ -41,6 +41,7 @@ app.use("/api/folders", authenticate, require("./routes/folder"));
 app.use("/api/servers", authenticate, require("./routes/server"));
 app.use("/api/pve-servers", authenticate, require("./routes/pveServer"));
 app.use("/api/identities", authenticate, require("./routes/identity"));
+app.use("/api/snippets", authenticate, require("./routes/snippet"));
 
 app.ws("/api/apps/installer", require("./routes/appInstaller"));
 app.use("/api/apps", authenticate, require("./routes/apps"));
