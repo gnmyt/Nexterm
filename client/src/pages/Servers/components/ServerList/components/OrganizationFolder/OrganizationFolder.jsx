d24f2fdf3c0c1adc8ee39e403ade1b440b9138f1
Mathias Wagner <germannewsmaker@gmail.com>
2025-03-15 14:10:11 +0100

    Create OrganizationFolder component

---
+++ client/src/pages/Servers/components/ServerList/components/OrganizationFolder/OrganizationFolder.jsx
@@ -0,0 +1,33 @@
+import Icon from "@mdi/react";
+import { mdiDomain, mdiDomainOff } from "@mdi/js";
+import ServerEntries from "../ServerEntries.jsx";
+import { useState } from "react";
+
+const OrganizationFolder = ({ id, name, entries, nestedLevel, connectToServer, connectToPVEServer, setRenameStateId, sshOnly }) => {
+    const [isOpen, setIsOpen] = useState(true);
+    const toggleFolder = () => setIsOpen(!isOpen);
+    const orgId = id.split('-')[1];
+
+    return (
+        <>
+            <div className="folder-object" onClick={toggleFolder}
+                 style={{ paddingLeft: `${10 + (nestedLevel * 15)}px` }}>
+                <Icon path={isOpen ? mdiDomain : mdiDomainOff} />
+                <p className="truncate-text">{name}</p>
+            </div>
+            {isOpen && (
+                <ServerEntries
+                    entries={entries}
+                    nestedLevel={nestedLevel + 1}
+                    setRenameStateId={setRenameStateId}
+                    folderId={`org-${orgId}`}
+                    connectToServer={connectToServer}
+                    connectToPVEServer={connectToPVEServer}
+                    sshOnly={sshOnly}
+                />
+            )}
+        </>
+    );
+};
+
+export default OrganizationFolder;
