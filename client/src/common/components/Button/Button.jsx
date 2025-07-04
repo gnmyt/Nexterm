8cc6777f04641d2dc16125ba2d09280f9f994cfe
Mathias Wagner <germannewsmaker@gmail.com>
2025-07-04 21:20:14 +0200

    Add buttonType prop to Button component for better form handling

--- client/src/common/components/Button/Button.jsx
+++ client/src/common/components/Button/Button.jsx
@@ -1,9 +1,9 @@
 import "./styles.sass";
 import Icon from "@mdi/react";
 
-export const Button = ({onClick, text, icon, disabled, type}) => {
+export const Button = ({onClick, text, icon, disabled, type, buttonType}) => {
     return (
-        <button className={"btn" + (type ? " type-" + type : "")} onClick={onClick} disabled={disabled}>
+        <button className={"btn" + (type ? " type-" + type : "")} onClick={onClick} disabled={disabled} type={buttonType}>
             {icon ? <Icon path={icon} /> : null}
             <h3>{text}</h3>
         </button>
