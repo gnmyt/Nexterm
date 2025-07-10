b05a14a0a9699b479cb40b15506d23a32b0dfb71
Mathias Wagner <germannewsmaker@gmail.com>
2025-07-10 20:18:02 +0200

    Fix access control in wsAuth.js util

--- server/utils/wsAuth.js
+++ server/utils/wsAuth.js
@@ -3,6 +3,7 @@ const Account = require("../models/Account");
 const Server = require("../models/Server");
 const Identity = require("../models/Identity");
 const prepareSSH = require("./prepareSSH");
+const { validateServerAccess } = require("../controllers/server");
 
 const authenticateWS = async (ws, req, options = {}) => {
     const { requiredParams = ['sessionToken', 'serverId'] } = options;
@@ -30,9 +31,15 @@ const authenticateWS = async (ws, req, options = {}) => {
         return null;
     }
 
-    const server = await Server.findOne({ where: { id: req.query.serverId, accountId: user.id } });
+    const server = await Server.findByPk(req.query.serverId);
     if (!server) {
         ws.close(4006, "The server does not exist");
+        return null;
+    }
+
+    const accessCheck = await validateServerAccess(user.id, server);
+    if (!accessCheck.valid) {
+        ws.close(4005, "You don't have access to this server");
         return null;
     }
 
