ea194376c5913d3d4517bd16ff213da3fc85ae3a
Mathias Wagner <germannewsmaker@gmail.com>
2025-07-13 10:14:39 +0200

    Integrate Audit page in Sidebar & App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import Settings from "@/pages/Settings";
 import Apps from "@/pages/Apps";
 import Snippets from "@/pages/Snippets";
 import Monitoring from "@/pages/Monitoring";
+import Audit from "@/pages/Audit";
 
 export const GITHUB_URL = "https://github.com/gnmyt/Nexterm";
 export const DISCORD_URL = "https://dc.gnmyt.dev/";
@@ -24,6 +25,7 @@ const App = () => {
                 { path: "/", element: <Navigate to="/servers" /> },
                 { path: "/servers", element: <Servers /> },
                 { path: "/monitoring", element: <Monitoring /> },
+                { path: "/audit", element: <Audit /> },
                 { path: "/settings/*", element: <Settings/> },
                 { path: "/apps/*", element: <Apps /> },
                 { path: "/snippets", element: <Snippets /> }
