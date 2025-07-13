d15a07d5ed471337fcdec583c7fc7d4612db716e
Mathias Wagner <germannewsmaker@gmail.com>
2025-07-13 09:55:58 +0200

    Create audit.js validation

---
+++ server/validations/audit.js
@@ -0,0 +1,22 @@
+const Joi = require("joi");
+
+module.exports.getAuditLogsValidation = Joi.object({
+    organizationId: Joi.number().integer().positive().optional(),
+    action: Joi.string().max(100).optional(),
+    resource: Joi.string().max(50).optional(),
+    startDate: Joi.date().iso().optional(),
+    endDate: Joi.date().iso().greater(Joi.ref('startDate')).optional(),
+    limit: Joi.number().integer().min(1).max(1000).default(50),
+    offset: Joi.number().integer().min(0).default(0),
+});
+
+module.exports.updateOrganizationAuditSettingsValidation = Joi.object({
+    requireConnectionReason: Joi.boolean().optional(),
+    enableFileOperationAudit: Joi.boolean().optional(),
+    enableServerConnectionAudit: Joi.boolean().optional(),
+    enableIdentityManagementAudit: Joi.boolean().optional(),
+    enableServerManagementAudit: Joi.boolean().optional(),
+    enableFolderManagementAudit: Joi.boolean().optional(),
+    enableScriptExecutionAudit: Joi.boolean().optional(),
+    enableAppInstallationAudit: Joi.boolean().optional(),
+}).min(1);
