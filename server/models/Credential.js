63fb96eb6f31ec5d4da38f872b2d8210fddd3db1
Mathias Wagner <germannewsmaker@gmail.com>
2025-11-19 19:21:56 +0100

    Create Credential.js model

---
+++ server/models/Credential.js
@@ -0,0 +1,73 @@
+const Sequelize = require("sequelize");
+const db = require("../utils/database");
+const { decrypt } = require("../utils/encryption");
+
+module.exports = db.define("credentials", {
+    identityId: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: {
+            model: "identities",
+            key: "id",
+        },
+        onDelete: "CASCADE",
+    },
+    integrationId: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: {
+            model: "integrations",
+            key: "id",
+        },
+        onDelete: "CASCADE",
+    },
+    serverEntryId: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: {
+            model: "entries",
+            key: "id",
+        },
+        onDelete: "CASCADE",
+    },
+    type: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    secretEncrypted: {
+        type: Sequelize.BLOB,
+        allowNull: false,
+    },
+    secretIV: {
+        type: Sequelize.STRING,
+        allowNull: true,
+    },
+    secretAuthTag: {
+        type: Sequelize.STRING,
+        allowNull: true,
+    },
+}, { 
+    freezeTableName: true,
+    timestamps: true,
+    hooks: {
+        afterFind: (credentials) => {
+            const decryptField = (obj) => {
+                if (obj.secretEncrypted) {
+                    try {
+                        obj.secret = decrypt(obj.secretEncrypted, obj.secretIV, obj.secretAuthTag);
+                    } catch (err) {
+                        console.error(`Failed to decrypt secret for credential ${obj.id}`);
+                    }
+                }
+            };
+
+            if (Array.isArray(credentials)) {
+                credentials.forEach((credential) => {
+                    decryptField(credential);
+                });
+            } else if (credentials) {
+                decryptField(credentials);
+            }
+        },
+    },
+});
