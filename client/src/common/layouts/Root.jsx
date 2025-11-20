0a28d1a76e0263929476d573dbd18020b55394e1
Mathias Wagner <germannewsmaker@gmail.com>
2025-11-20 20:17:49 +0100

    Create KeymapContext.jsx and integrate in Root.jsx

--- client/src/common/layouts/Root.jsx
+++ client/src/common/layouts/Root.jsx
@@ -5,12 +5,14 @@ import { IdentityProvider } from "@/common/contexts/IdentityContext.jsx";
 import { ToastProvider } from "@/common/contexts/ToastContext.jsx";
 import { TerminalSettingsProvider } from "@/common/contexts/TerminalSettingsContext.jsx";
 import { AIProvider } from "@/common/contexts/AIContext.jsx";
+import { KeymapProvider } from "@/common/contexts/KeymapContext.jsx";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { SessionProvider } from "@/common/contexts/SessionContext.jsx";
 import { SnippetProvider } from "@/common/contexts/SnippetContext.jsx";
 import { Suspense, lazy } from "react";
 import Loading from "@/common/components/Loading";
+import GlobalKeyboardHandler from "@/common/components/GlobalKeyboardHandler";
 
 const Sidebar = lazy(() => import("@/common/components/Sidebar"));
 
@@ -20,26 +22,29 @@ export default () => {
             <ToastProvider>
                 <TerminalSettingsProvider>
                     <UserProvider>
-                        <AIProvider>
-                            <ServerProvider>
-                                <IdentityProvider>
-                                    <SnippetProvider>
-                                        <SessionProvider>
-                                            <div className="content-wrapper">
-                                                <Suspense fallback={<Loading />}>
-                                                    <Sidebar />
-                                                </Suspense>
-                                                <div className="main-content">
+                        <KeymapProvider>
+                            <AIProvider>
+                                <ServerProvider>
+                                    <IdentityProvider>
+                                        <SnippetProvider>
+                                            <SessionProvider>
+                                                <GlobalKeyboardHandler />
+                                                <div className="content-wrapper">
                                                     <Suspense fallback={<Loading />}>
-                                                        <Outlet />
+                                                        <Sidebar />
                                                     </Suspense>
+                                                    <div className="main-content">
+                                                        <Suspense fallback={<Loading />}>
+                                                            <Outlet />
+                                                        </Suspense>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                        </SessionProvider>
-                                    </SnippetProvider>
-                                </IdentityProvider>
-                            </ServerProvider>
-                        </AIProvider>
+                                            </SessionProvider>
+                                        </SnippetProvider>
+                                    </IdentityProvider>
+                                </ServerProvider>
+                            </AIProvider>
+                        </KeymapProvider>
                     </UserProvider>
                 </TerminalSettingsProvider>
             </ToastProvider>
