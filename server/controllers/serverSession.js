b5d78967c121fb6740745820492e5cc378e178a0
Mathias Wagner <germannewsmaker@gmail.com>
2025-11-23 16:01:01 +0100

    Upgrade SFTP experience

--- server/controllers/serverSession.js
+++ server/controllers/serverSession.js
@@ -4,6 +4,7 @@ const Account = require("../models/Account");
 const { validateEntryAccess } = require("./entry");
 const { getOrganizationAuditSettingsInternal } = require("./audit");
 const { resolveIdentity } = require("../utils/identityResolver");
+const Organization = require('../models/Organization');
 
 const createSession = async (accountId, entryId, identityId, connectionReason, type = null, directIdentity = null, tabId = null, browserId = null) => {
     const entry = await Entry.findByPk(entryId);
@@ -66,10 +67,28 @@ const getSessions = async (accountId, tabId = null, browserId = null) => {
 
     const sessions = SessionManager.getAll(accountId, filterTabId, filterBrowserId);
     logger.info('Sessions found', { count: sessions.length });
-    return sessions.map(session => {
+    
+
+    return await Promise.all(sessions.map(async (session) => {
+        const entry = await Entry.findByPk(session.entryId, {
+            attributes: ['id', 'organizationId']
+        });
+
+        let organizationName = null;
+        if (entry?.organizationId) {
+            const org = await Organization.findByPk(entry.organizationId, {
+                attributes: ['name']
+            });
+            organizationName = org?.name || null;
+        }
+
         const { connection, ...safeSession } = session;
-        return safeSession;
-    });
+        return {
+            ...safeSession,
+            organizationId: entry?.organizationId || null,
+            organizationName
+        };
+    }));
 };
 
 const hibernateSession = (sessionId) => {
