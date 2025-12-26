8dfad543f75cab6352189a0110782d5e5ce88564
Mathias Wagner <germannewsmaker@gmail.com>
2025-12-26 20:02:16 +0100

    Update Button to support icon-only

--- client/src/common/components/Button/Button.jsx
+++ client/src/common/components/Button/Button.jsx
@@ -3,9 +3,9 @@ import Icon from "@mdi/react";
 
 export const Button = ({onClick, text, icon, disabled, type, buttonType}) => {
     return (
-        <button className={"btn" + (type ? " type-" + type : "")} onClick={onClick} disabled={disabled} type={buttonType}>
+        <button className={"btn" + (type ? " type-" + type : "") + (!text ? " icon-only" : "")} onClick={onClick} disabled={disabled} type={buttonType}>
             {icon ? <Icon path={icon} /> : null}
-            <h3>{text}</h3>
+            {text && <h3>{text}</h3>}
         </button>
     );
 }
