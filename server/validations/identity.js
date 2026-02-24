a0c67e6c8a436ea7de2a96a67a9e38fc378d0efc
Eduardo Oliveira <eduardo.oliveira@virtueyes.com.br>
2026-02-24 18:21:12 -0300

    Update identity.js

--- server/validations/identity.js
+++ server/validations/identity.js
@@ -1,7 +1,7 @@
 const Joi = require("joi");
 module.exports.createIdentityValidation = Joi.object({
     name: Joi.string().min(3).max(255).required(),
-    username: Joi.string().max(255),
+    username: Joi.string().max(255).optional(),
     type: Joi.string().valid("password", "ssh", "both", "password-only").required(),
     password: Joi.string().optional(),
     sshKey: Joi.string().optional(),
@@ -11,11 +11,11 @@ module.exports.createIdentityValidation = Joi.object({
 
 module.exports.updateIdentityValidation = Joi.object({
     name: Joi.string().min(3).max(255).optional(),
-    username: Joi.string().max(255),
+    username: Joi.string().max(255).optional(),
     type: Joi.string().valid("password", "ssh", "both", "password-only").optional(),
     password: Joi.string().optional(),
     sshKey: Joi.string().optional(),
-    passphrase: Joi.string().optional(),
+    passphrase: Joi.string().allow("").optional(),
     organizationId: Joi.number().integer().optional(),
 }).or("name", "username", "type", "password", "sshKey", "passphrase", "organizationId");
 
