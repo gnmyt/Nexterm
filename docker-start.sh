e314b407b5d1f645b41c1e55fd80b4cd9599bdf7
Mathias Wagner <germannewsmaker@gmail.com>
2026-02-26 18:52:46 +0100

    Remove guacd from docker-start.sh

--- docker-start.sh
+++ docker-start.sh
@@ -1,27 +1,3 @@
 #!/bin/bash
 
-GUACD_LOG_LEVEL=${LOG_LEVEL:-system}
-
-case "$GUACD_LOG_LEVEL" in
-    error)
-        GUACD_ARGS="-L error"
-        ;;
-    warn)
-        GUACD_ARGS="-L warning"
-        ;;
-    system|info)
-        GUACD_ARGS="-L info"
-        ;;
-    verbose)
-        GUACD_ARGS="-L debug"
-        ;;
-    debug)
-        GUACD_ARGS="-L trace"
-        ;;
-    *)
-        GUACD_ARGS="-L info"
-        ;;
-esac
-
-guacd -b 0.0.0.0 -l 4822 $GUACD_ARGS -f &
 exec node server/index.js
