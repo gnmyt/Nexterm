1d94be6c61cf2e3b19ef917800b51e901533a973
Mathias Wagner <germannewsmaker@gmail.com>
2026-07-16 18:49:12 +0200

    Fix schema.js

--- server/utils/schema.js
+++ server/utils/schema.js
@@ -1,8 +1,12 @@
 module.exports.validateSchema = (res, schema, object) => {
-    const { error } = schema.validate(object, { errors: { wrap: { label: "" } }, allowUnknown: false });
-    const message = error?.details[0].message || "No message provided";
+    const { error, value } = schema.validate(object, { errors: { wrap: { label: "" } }, allowUnknown: false });
 
-    if (error) res.status(400).json({ message });
+    if (error) {
+        res.status(400).json({ message: error.details[0].message || "No message provided" });
+        return error;
+    }
+
+    if (object !== null && typeof object === "object") Object.assign(object, value);
 
     return error;
 };
