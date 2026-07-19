ee53a18c90f3d8ca2084ad2c469e42628d555f31
Mathias Wagner <germannewsmaker@gmail.com>
2026-07-19 13:41:41 +0200

    Craete sessionParticipant.js util

---
+++ server/utils/sessionParticipant.js
@@ -0,0 +1,18 @@
+const SessionManager = require("../lib/SessionManager");
+
+const buildParticipant = (ctx) => ({
+    accountId: ctx.user?.id || null,
+    username: ctx.user?.username || null,
+    firstName: ctx.user?.firstName || null,
+    lastName: ctx.user?.lastName || null,
+    kind: !ctx.isShared ? "owner" : ctx.isOrgJoin ? "organization" : "link",
+    writable: !ctx.isShared ? true : ctx.shareWritable === true,
+});
+
+const createWriteGuard = (ctx, sessionId) => {
+    if (!ctx.isShared) return () => true;
+    if (ctx.isOrgJoin) return () => ctx.shareWritable === true;
+    return () => SessionManager.get(sessionId)?.shareWritable === true;
+};
+
+module.exports = { buildParticipant, createWriteGuard };
